bdb995476d157f65135006147f68591e0a673dfa
Eric Laursen <ericjlaursen@gmail.com>
2018-04-04 10:44:40 -0400

    First submit of webClone respoinsive map.

---
+++ responsive/state/webClone/index.js
@@ -0,0 +1,26 @@
+  d3.select(window)
+        .on("resize", sizeChange);
+
+  var projection = d3.geo.albersUsa()
+    .scale(1100);
+
+  var path = d3.geo.path()
+    .projection(projection);
+
+  var svg = d3.select("#container")
+    .append("svg")
+    .attr("width", "100%")
+        .append("g");
+  
+  d3.json("data.json", function(error, us) {
+    svg.selectAll(".states")
+    .data(topojson.object(us, us.objects.states).geometries)
+   .enter().append("path")
+    .attr("class", "states")
+    .attr("d", path);
+  });
+
+  function sizeChange() {
+      d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
+      $("svg").height($("#container").width()*0.618);
+  }
