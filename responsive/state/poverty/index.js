c3f1b1740c69afb19c83246014e668acd38d6cdd
Eric Laursen <ericjlaursen@gmail.com>
2018-04-11 07:08:47 -0400

    Copies JS from responsive webClone.

--- responsive/state/poverty/index.js
+++ responsive/state/poverty/index.js
@@ -1,4 +1,3 @@
-
 // color 
 let lvl = {
     "one":5,
@@ -6,12 +5,17 @@ let lvl = {
     "three":15,
     "four":20
 }
-var income_domain = [lvl.one,lvl.two,lvl.three,lvl.four]
-var income_color = d3.scaleThreshold()
-    .domain(income_domain)
-    .range(d3.schemeReds[5]);
 
-// incomeData 
+var income_domain = [lvl.one,lvl.two,lvl.three,lvl.four]
+
+//categorical coloring option
+// var income_color = d3.scaleThreshold()
+//     .domain(income_domain)
+//     .range(d3.schemeReds[5]);
+
+var income_color = d3.scaleSequential(d3.interpolateReds)
+    .domain([.5,31.9])
+
 var incomeData = d3.map();
 
 
@@ -22,6 +26,7 @@ let colorRatio = {
     "level4": 0,
     "level5": 0
 }
+
 
 // asynchronous tasks, load topojson maps and data
 d3.queue()
@@ -54,12 +59,17 @@ d3.queue()
     .await(ready);
 
 
+d3.select(window)
+      .on("resize", sizeChange);
 
-// callback function  
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("class", 'income')
+      .append("g");
+
+
 function ready(error, data) {
-    //NOTE: this 'data' is the CTstate 'data'
-    //      NOT the data.csv 'data'
-
     if (error) throw error;
 
     // connecticut topojson
@@ -70,23 +80,23 @@ function ready(error, data) {
 
     // projection and path
     var projection = d3.geoAlbersUsa()
-        .fitExtent([[20, 20], [700, 580]], connecticut);
+        .fitExtent([[0,0], [900, 550]], connecticut);
 
     var geoPath = d3.geoPath()
         .projection(projection);
 
     // draw connecticut map and bind income data
-    d3.select("svg.income").selectAll("path")
+    svg.selectAll(".towns")
         .data(connecticut.features)
-        .enter()
-        .append("path")
+        .enter().append("path")
+        .attr("class", "towns")        
         .attr("d", geoPath)
-        .attr("fill", "white")
-        .transition().duration(100)
-        .delay(function(d, i) {
-            return i * 2; 
-        })
-        .ease(d3.easeLinear)
+        // .attr("fill", "white")
+        // .transition().duration(100)
+        // .delay(function(d, i) {
+        //     return i * 2; 
+        // })
+        // .ease(d3.easeLinear)
         .attr("fill", function(d) { 
             var townGeoID = incomeData.get(d.properties.GEOID10);
             return (
@@ -94,8 +104,7 @@ function ready(error, data) {
                 income_color(townGeoID) : 
                 "lightblue");  
 
-        })
-        .attr("class", "counties-income");
+        });
     
     // title
     d3.select("svg.income").selectAll("path")
@@ -106,3 +115,12 @@ function ready(error, data) {
 
 }
 
+function sizeChange() {
+    d3
+      .select("g")
+      .attr('transform', 'translate(100,50)')
+      .attr("transform", "scale(" + $("#container")
+      .width()/900 + ")");
+   
+    $("svg").height($("#container").width()*0.618);
+}
