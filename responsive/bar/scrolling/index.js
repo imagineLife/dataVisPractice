8bfe4c1798208d691f63572ea4386b697585ea1e
Eric Laursen <ericjlaursen@gmail.com>
2018-07-22 17:44:40 -0400

    Code cleanup.

--- responsive/bar/scrolling/index.js
+++ responsive/bar/scrolling/index.js
@@ -10,9 +10,8 @@ function makeLinearScale(domMin,domMax,rangeMin,rangeMax){
         .range([rangeMin, rangeMax]);
 }
 
-var x,y,gX, gY, d3xAxis, d3yAxis; 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
-var mainData;
+var xScale,yScale,gXObj, gYObj, d3xAxis, d3yAxis; 
+
 var settings = {
     targets:[],
     detail:{
@@ -22,17 +21,15 @@ var settings = {
 d3.json("data.json",function(data){
 
     if(data[0].metric.WIDGET_SETTINGS != ""){
-      console.log('here!')
         var wid = JSON.parse(data[0].metric.WIDGET_SETTINGS);   
         if(wid != null){
           $.extend(settings,wid);
         }
     }
     
-    mainData = data;
     var svg = d3.select("svg");
 
-    var limits = {maxY:null, minY:null, maxX:null, minX:null};
+    var dataLimits = {maxY:null, minY:null, maxX:null, minX:null};
     var padding = {top:20,bottom:150,left:100,right:20};
 
     var svgWidth = +svg.attr("width");
@@ -42,125 +39,100 @@ d3.json("data.json",function(data){
     var canvasWidth  = svgWidth-padding.left-padding.right;
 
     data.forEach(function(e,i) {        
-        var eMaxY = d3.max(e.data,function(d){return +d.VALUE_NUMERIC;});
-        var eMinY = d3.min(e.data,function(d){return +d.VALUE_NUMERIC;});
-        var eMaxX = d3.max(e.data,function(d){return new Date(d.DATA_DATE);});
-        var eMinX = d3.min(e.data,function(d){return new Date(d.DATA_DATE);});
+      var eMaxY = d3.max(e.data,function(d){return +d.VALUE_NUMERIC;});
+      var eMinY = d3.min(e.data,function(d){return +d.VALUE_NUMERIC;});
+      var eMaxX = d3.max(e.data,function(d){return new Date(d.DATA_DATE);});
+      var eMinX = d3.min(e.data,function(d){return new Date(d.DATA_DATE);});
 
-        if(limits.maxX == null){ limits.maxX = eMaxX;} 
-        else { if(eMaxX > limits.maxX){ limits.maxX = eMaxX;}}
+      if(dataLimits.maxX == null){ dataLimits.maxX = eMaxX;} 
+      else { if(eMaxX > dataLimits.maxX){ dataLimits.maxX = eMaxX;}}
 
-        if(limits.minX == null){ limits.minX = eMinX;} 
-        else { if(eMinX < limits.minX){ limits.minX = eMinX;}}
+      if(dataLimits.minX == null){ dataLimits.minX = eMinX;} 
+      else { if(eMinX < dataLimits.minX){ dataLimits.minX = eMinX;}}
 
-        if(limits.maxY == null){limits.maxY = eMaxY;} 
-        else { if(eMaxY > limits.maxY){limits.maxY = eMaxY;}}
+      if(dataLimits.maxY == null){dataLimits.maxY = eMaxY;} 
+      else { if(eMaxY > dataLimits.maxY){dataLimits.maxY = eMaxY;}}
 
-        if(limits.minY == null){limits.minY = eMinY;}
-        else { if(eMinY < limits.minY){limits.minY = eMinY;}}
+      if(dataLimits.minY == null){dataLimits.minY = eMinY;}
+      else { if(eMinY < dataLimits.minY){dataLimits.minY = eMinY;}}
     });   
 
-    settings.targets.forEach(function(d){
-        if(limits.maxY < d.value){
-            limits.maxY = d.value;
-        } 
-        if(limits.minY > d.value){
-            limits.minY = d.value;
-        }
-    });
-
-
     var gWrapper = svg.append("g")
-                    .attrs({
-                      "id" : "gWrapper",
-                      "width" : canvasWidth,
-                      "height" : canvasHeight,
-                      "transform" : "translate("+padding.left+","+padding.top+")"
-                    });
+        .attrs({
+          "id" : "gWrapper",
+          "width" : canvasWidth,
+          "height" : canvasHeight,
+          "transform" : "translate("+padding.left+","+padding.top+")"
+        });
 
+    xScale = makeTimeScale(dataLimits.minX, dataLimits.maxX, 0,+gWrapper.attr("width"));
 
-    x = makeTimeScale(limits.minX, limits.maxX, 0,+gWrapper.attr("width"));
+    yScale = makeLinearScale( dataLimits.maxY*1.1, dataLimits.minY-(dataLimits.minY*0.1), 0,+gWrapper.attr("height"))
 
-    y = makeLinearScale( limits.maxY*1.1, limits.minY-(limits.minY*0.1), 0,+gWrapper.attr("height"))
-
-    var zoom = d3.zoom().on("zoom",zoomed);
+    var d3Zoom = d3.zoom().on("zoom",zoomed);
   
-    d3xAxis = d3.axisBottom(x);
-    d3yAxis = d3.axisLeft(y);
-
-    gWrapper.selectAll(".targets")
-          .data(settings.targets)
-          .enter()
-            .append("line")
-            .classed("targets",true)
-            .style("stroke",function(d){return d.color;})
-            .style("stroke-width",1)
-            .attrs({
-              "x1" : x(limits.minX),
-              "x2" : x(limits.maxX),
-              "y1" : (d) => y(+d.value),
-              "y2" : (d) => y(+d.value)
-            })
+    d3xAxis = d3.axisBottom(xScale);
+    d3yAxis = d3.axisLeft(yScale);
 
     var clip = gWrapper.append("clipPath")
-                    .attr("id","clip")
-                    .append("rect")
-                    .attrs({
-                      "width":canvasWidth,
-                      "height" : canvasHeight
-                    });
-    
-    gX = gWrapper.append("g")
-                .attrs({
-                  "transform" : "translate(0,"+(+gWrapper.attr("height"))+")",
-                  "class" : "axis axis--x"
-                })
-                .call(d3xAxis);
+      .attr("id","clip")
+      .append("rect")
+      .attrs({
+        "width":canvasWidth,
+        "height" : canvasHeight,
+        "class": 'clipClass'
+      });
 
-    gY = gWrapper.append("g").attr("class","axis axis--y").call(d3yAxis);
+    gXObj = gWrapper.append("g")
+      .attrs({
+        "transform" : "translate(0,"+(+gWrapper.attr("height"))+")",
+        "class" : "axis axis--x"
+      })
+      .call(d3xAxis);
+
+    gYObj = gWrapper.append("g").attr("class","axis axis--y").call(d3yAxis);
     
     d3.selectAll(".axis--y > g.tick > line").attr("x2",canvasWidth).style("stroke","lightgrey");
 
-    if(settings.detail.type == "bar"){
-        barWidth = (x(new Date("2016-01-02")) - x(new Date("2016-01-01")));
-        var barLines = gWrapper.selectAll("rect.bar")
-            .data(data[0].data)    
-            .enter()
-            .append("rect")
-            .attrs({
-              "class" : "bar",
-              "clip-path" :  "url(#clip)",
-              "x" : (d) => x(new Date(d.DATA_DATE))-barWidth*0.5,
-              "width" : barWidth,
-              "height" : (d) => canvasHeight-y(d.VALUE_NUMERIC),
-              "y" : (d) =>  y(d.VALUE_NUMERIC)
-            })
-            .style("fill","steelblue")
-            .style("stroke","blue")
-            .style("stroke-width","1px");
-    }
+    barWidth = (xScale(new Date("2016-01-02")) - xScale(new Date("2016-01-01")));
+    
+    var barLines = gWrapper.selectAll("rect.bar")
+      .data(data[0].data)    
+      .enter()
+      .append("rect")
+      .attrs({
+        "class" : "barClass",
+        "clip-path" :  "url(#clip)",
+        "x" : (d) => xScale(new Date(d.DATA_DATE))-barWidth*0.5,
+        "width" : barWidth,
+        "height" : (d) => canvasHeight-yScale(d.VALUE_NUMERIC),
+        "y" : (d) =>  yScale(d.VALUE_NUMERIC)
+      })
+      .style("fill","steelblue")
+      .style("stroke","blue")
+      .style("stroke-width","1px");
    
 
     gWrapper.append("g")
-          .attr("transform","translate("+(-40)+","+
-          (+gWrapper.attr("height"))/2
-          +") rotate(270)")
-          .append("text")
-          .attr("text-anchor","middle")
-          .text(data[0].metric.Y_AXIS_NAME);
+      .attr("transform", `translate( ${(-40)} , ${(+gWrapper.attr("height"))/2 }) rotate(270)`)
+      .append("text")
+      .attrs({
+        "text-anchor" : "middle",
+        'class' : 'rotated'
+      })
+      .text(data[0].metric.Y_AXIS_NAME);
 
-    svg.call(zoom);
+    svg.call(d3Zoom);
 });
 
 function zoomed() {
-    gX.call(d3xAxis.scale(d3.event.transform.rescaleX(x)));
-    var rescaleXFn = d3.event.transform.rescaleX(x);
+    gXObj.call(d3xAxis.scale(d3.event.transform.rescaleX(xScale)));
+    var rescaleXFn = d3.event.transform.rescaleX(xScale);
  
     barWidth = rescaleXFn(new Date("2016-01-02")) - rescaleXFn(new Date("2016-01-01"));
-    d3.select("#gWrapper").selectAll("rect.bar")
-        .data(mainData[0].data)    
-        .attrs({
-          "x" : (d) => rescaleXFn(new Date(d.DATA_DATE))-barWidth*0.5,
-          "width" : barWidth
-        });
+    d3.selectAll("rect.barClass")   
+      .attrs({
+        "x" : (d) => rescaleXFn(new Date(d.DATA_DATE))-barWidth*0.5,
+        "width" : barWidth
+      });
 }
