ad6cd68a3a86133d09c7bfd06b38a1982921de70
Eric Laursen <ericjlaursen@gmail.com>
2018-07-30 20:26:48 -0400

    Code Cleanup, functionalizing.

--- responsive/bar/scrolling/index.js
+++ responsive/bar/scrolling/index.js
@@ -4,7 +4,52 @@ function makeLinearScale(domMin,domMax,rangeMin,rangeMax){
         .range([rangeMin, rangeMax]);
 }
 
-var xScale,yScale,gXObj, gYObj, d3xAxis, d3yAxis; 
+function zoomed() {  
+  var rescaleXFn = d3.event.transform.rescaleX(xScale);
+
+  gXObj.call(d3xAxis.scale(rescaleXFn));
+
+  barWidth = ( rescaleXFn(2) - rescaleXFn(1) ) * .5;
+
+  d3.selectAll("rect.barClass")   
+    .attrs({
+      "x" : (d) => rescaleXFn(d.measureNumber)-barWidth*.5,
+      "width" : barWidth
+    });
+}
+
+function makeClipPath(parent, id, w, h, cl){
+  return parent.append("clipPath")
+  .attr("id", id)
+  .append("rect")
+  .attrs({
+    "width":w,
+    "height" : h,
+    "class": cl
+  });
+}
+
+function makeAxisGWrapper(parent, transformation, className, axisObj){
+  return parent.append("g")
+  .attrs({
+    "transform" : transformation,
+    "class" : className
+  })
+  .call(axisObj);
+}
+
+function makeAxisLabel(parent, transformation, className, textVal){
+  return parent.append("g")
+  .attr('transform', transformation)
+  .append('text')
+  .attrs({
+    'text-anchor' : 'middle',
+    'class' : className
+  })
+  .text(textVal);
+}
+
+let xScale,yScale,gXObj, gYObj, d3xAxis, d3yAxis; 
 
 var settings = {
     targets:[],
@@ -13,33 +58,37 @@ var settings = {
     }
 };
 
+
+
 d3.json("npm.json", function(data){
     
     var svg = d3.select("svg");
 
     var dataLimits = {maxY:14, minY:0, maxX:157, minX:0};
-    var padding = {top:20,bottom:150,left:100,right:20};
+    var margin = {top:20,bottom:150,left:100,right:20};
 
     var svgWidth = +svg.attr("width");
     var svgHeight = +svg.attr("height");
 
-    var canvasHeight = svgHeight-padding.top-padding.bottom;
-    var canvasWidth  = svgWidth-padding.left-padding.right;
+    var gWrapperHeight = svgHeight-margin.top-margin.bottom;
+    var gWrapperWidth  = svgWidth-margin.left-margin.right;
 
     data.forEach((d,i) => {
-      var eMaxY = d3.max(d,function(d){return +d.howMany;});
-      var eMinY = d3.min(d,function(d){return +d.howMany;});
-      var eMaxX = d3.max(d,function(d){return +d.measureNumber;});
-      var eMinX = d3.min(d,function(d){return +d.measureNumber;});
+      var maxY = d3.max(d,function(d){return +d.howMany;});
+      var minY = d3.min(d,function(d){return +d.howMany;});
+      var maxX = d3.max(d,function(d){return +d.measureNumber;});
+      var minX = d3.min(d,function(d){return +d.measureNumber;});
     });   
 
     var gWrapper = svg.append("g")
         .attrs({
           "id" : "gWrapper",
-          "width" : canvasWidth,
-          "height" : canvasHeight,
-          "transform" : "translate("+padding.left+","+padding.top+")"
+          "width" : gWrapperWidth,
+          "height" : gWrapperHeight,
+          "transform" : `translate( ${margin.left}, ${margin.top})`
         });
+
+    var clipObj = makeClipPath(gWrapper, "clipObj", gWrapperWidth, gWrapperHeight, 'clipClass');
 
     xScale = makeLinearScale(0, 12, 0,+gWrapper.attr("width"));
 
@@ -51,80 +100,34 @@ d3.json("npm.json", function(data){
 
     d3yAxis = d3.axisLeft(yScale);
 
-    var clip = gWrapper.append("clipPath")
-      .attr("id","clip")
-      .append("rect")
-      .attrs({
-        "width":canvasWidth,
-        "height" : canvasHeight,
-        "class": 'clipClass'
-      });
+    let gXObj = makeAxisGWrapper(gWrapper, `translate(0, ${gWrapper.attr('height')})`, 'axis axis--x', d3xAxis);
 
-    gXObj = gWrapper.append("g")
-      .attrs({
-        "transform" : `translate(0, ${gWrapper.attr("height")})`,
-        "class" : "axis axis--x"
-      })
-      .call(d3xAxis);
-
-    gYObj = gWrapper.append("g")
-      .attr("class","axis axis--y")
-      .call(d3yAxis);
+    let gXgYObjObj = makeAxisGWrapper(gWrapper, `translate(0, 0)`, 'axis axis--y', d3yAxis);
     
     d3.selectAll(".axis--y > g.tick > line")
-      .attr("x2",canvasWidth)
+      .attr("x2",gWrapperWidth)
+      .attr('class', 'xGridLine')
       .style("stroke","lightgrey");
 
     barWidth = ( xScale(2) - xScale(1) ) * .5;
     
-    var barLines = gWrapper.selectAll("rect.bar")
+    var bars = gWrapper.selectAll("rect.bar")
       .data(data)    
       .enter()
       .append("rect")
       .attrs({
         "class" : 'barClass',
-        "clip-path" :  'url(#clip)',
+        "clip-path" :  'url(#clipObj)',
         "x" : (d) => xScale(d.measureNumber)-barWidth*.5,
         "width" : barWidth,
-        "height" : (d) => canvasHeight-yScale(d.howMany),
+        "height" : (d) => gWrapperHeight-yScale(d.howMany),
         "y" : (d) =>  yScale(d.howMany)
       })
       .style('fill','steelblue')
-   
 
-    let xAxisLabel = gWrapper.append("g")
-      .attr('transform', `translate( ${ gWrapper.attr('width')/2 } , ${ svg.attr('height') * .9 } )`)
-      .append('text')
-      .attrs({
-        'text-anchor' : 'middle',
-        'class' : 'rotated'
-      })
-      .text('Measure Number');
+    let xAxisLabel = makeAxisLabel(gWrapper, `translate( ${ gWrapper.attr('width')/2 } , ${ svg.attr('height') * .9 } )`, 'xAxis label', 'Measure Number')
 
-    let yAxisLabel = gWrapper.append('g')
-      .attr('transform', `translate( ${(-40)} , ${(+gWrapper.attr('height'))/2 }) rotate(270)`)
-      .append('text')
-      .attrs({
-        'text-anchor' : 'middle',
-        'class' : 'rotated'
-      })
-      .text('Note Count');
+    let yAxisLabel = makeAxisLabel(gWrapper, `translate( ${(-40)} , ${(+gWrapper.attr('height'))/2 }) rotate(270)`, 'yAxis label', 'Note Count')
 
     svg.call(d3Zoom);
 });
-
-function zoomed() {
-    
-    var rescaleXFn = d3.event.transform.rescaleX(xScale);
-
-    gXObj.call(d3xAxis.scale(rescaleXFn));
-
- 
-    barWidth = ( rescaleXFn(2) - rescaleXFn(1) ) * .5;
-
-    d3.selectAll("rect.barClass")   
-      .attrs({
-        "x" : (d) => rescaleXFn(d.measureNumber)-barWidth*.5,
-        "width" : barWidth
-      });
-}
