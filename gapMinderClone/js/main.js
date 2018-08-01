b7b8136fd4335ab19f996a03a16896785dd2b7d7
Eric Laursen <ericjlaursen@gmail.com>
2018-07-31 21:31:23 -0400

    Builds scales.

--- gapMinderClone/js/main.js
+++ gapMinderClone/js/main.js
@@ -23,15 +23,48 @@ function appendD3Element(parent, type, className){
 	return parent.append(type).attr('class', className)
 }
 
+function makeLogScale(baseVal, domainArr, rangeArr ){
+	return d3.scaleLog()
+		.base(baseVal)
+		.domain(domainArr)
+		.range(rangeArr);
+}
+
+function makeLinearScale(domainArr, rangeArr){
+	return d3.scaleLinear()
+		.domain(domainArr)
+		.range(rangeArr);
+}
+
 d3.json("data/data.json").then(function(data){
-	console.log(data);
+
+	let timeVar = 0;
+
+	//domain & range vars
+	const logDomain = [142, 150000],
+		logRange = [0, v.setWidth],
+		linearRange = [v.setHeight],
+		linearDomain = [0,90],
+		radiusDom = [2000,1400000000],
+		radiusRange = [25*Math.PI, 1500*Math.PI];
 	
 	//calculate svg dimensions less margins
 	const heightLessMargins = v.setHeight - v.margin.t - v.margin.b;
 	const widthLessMargins = v.setWidth - v.margin.l - v.margin.r;
 
+	//make svg & g objects
 	let svgObj = appendElement('#chart','svg',heightLessMargins, widthLessMargins, 'svgObj');
 	let gObj = appendD3Element(svgObj, 'g', 'gWrapper');
-	gObj.attr('transform', `translate( ${v.margin.l}, ${v.margin.t}`);
+	gObj.attr('transform', `translate( ${v.margin.l}, ${v.margin.t})`);
+
+	//make x & y & circle-radius scales
+	let xScale = makeLogScale(10, logDomain, logRange);
+	let yScale = makeLinearScale(linearDomain, linearRange);
+	let radiusScale = makeLinearScale(radiusDom, radiusRange);
+
+
+
+
+	
 
 })
