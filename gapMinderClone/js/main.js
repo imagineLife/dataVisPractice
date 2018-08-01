c40d0cffa427fd7d29605a80b67a3991237314e6
Eric Laursen <ericjlaursen@gmail.com>
2018-08-01 12:49:22 -0400

    adjusts axis placements.

--- gapMinderClone/js/main.js
+++ gapMinderClone/js/main.js
@@ -2,7 +2,7 @@ const v = {
 	margin: {
 		t: 50,
 		r: 20,
-		b: 100,
+		b: 50,
 		l: 80
 	},
 	setHeight: 500,
@@ -50,26 +50,45 @@ function makeLabel(parent, xVal, yVal, textVal){
 		.text(textVal)
 }
 
+function makeD3xAxis(scaleObj, tickVals, tickFormatFn){
+	return d3.axisBottom(scaleObj)
+		.tickValues(tickVals)
+		.tickFormat(tickFormatFn)
+}
+
+function makeD3yAxis(scaleObj){
+	return d3.axisLeft(scaleObj)
+		.tickFormat(function(d){ return +d})
+}
+
+function connectAxisToParent(parent, transformation, className){
+	return parent.append('g')
+		.attrs({
+			'class': className,
+			'transform': transformation
+		})
+}
+
 d3.json("data/data.json").then(function(data){
 
 	let timeVar = 0;
 
+	//calculate svg dimensions less margins
+	const heightLessMargins = v.setHeight - v.margin.t - v.margin.b;
+	const widthLessMargins = v.setWidth - v.margin.l - v.margin.r;	
+
 	//domain & range vars
 	const logDomain = [142, 150000],
 		logRange = [0, v.setWidth],
-		linearRange = [v.setHeight],
+		linearRange = [heightLessMargins, 0],
 		linearDomain = [0,90],
 		radiusDom = [2000,1400000000],
 		radiusRange = [25*Math.PI, 1500*Math.PI];
-	
-	//calculate svg dimensions less margins
-	const heightLessMargins = v.setHeight - v.margin.t - v.margin.b;
-	const widthLessMargins = v.setWidth - v.margin.l - v.margin.r;
 
 	//make svg & g objects
-	let svgObj = appendElement('#chart','svg', widthLessMargins, heightLessMargins, 'svgObj');
-	let gObj = appendD3Element(svgObj, 'g', 'gWrapper');
-	gObj.attr('transform', `translate( ${v.margin.l}, ${v.margin.t})`);
+	let svgObj = appendElement('#chart','svg', v.setWidth, v.setHeight, 'svgObj');
+	let gWrapper = appendD3Element(svgObj, 'g', 'gWrapper');
+	gWrapper.attr('transform', `translate( ${v.margin.l}, ${v.margin.t})`);
 
 	//make x & y & circle-radius scales
 	let xScale = makeLogScale(10, logDomain, logRange);
@@ -78,11 +97,33 @@ d3.json("data/data.json").then(function(data){
 	const colorScale = d3.scaleOrdinal(d3.schemePasetl1)
 
 	//make axis labels
-	let xAxisLabel = makeLabel(gObj, ( widthLessMargins / 2 ), ( heightLessMargins - 50 ), v.xLabelText);
-	let yAxisLabel = makeLabel(gObj, ( -170 ), ( -40 ), v.yLabelText);
-	let timeAxisLabel = makeLabel(gObj, ( -40 ), ( -10 ), v.timeLabelText);
+	let xAxisLabel = makeLabel(gWrapper, ( widthLessMargins / 2 ), ( v.setHeight - 30 ), v.xLabelText);
+	let yAxisLabel = makeLabel(gWrapper, ( -170 ), ( -40 ), v.yLabelText);
+	let timeAxisLabel = makeLabel(gWrapper, ( -40 ), ( -10 ), v.timeLabelText);
 
 	//adjust labels
 	yAxisLabel.attr('transform','rotate(-90)')
+
+
+	//make axis objects
+	let xAxisObj = makeD3xAxis(xScale, [400, 4000, 40000], d3.format('$'))
+	let yAxisObj = makeD3yAxis(yScale)
+
+	//append axis to gWrapper
+	let transformString = "translate(0," + (heightLessMargins) +")";
+
+	let xAxisG = connectAxisToParent(gWrapper, transformString, 'xAxisG');
+		xAxisG.call(xAxisObj)
+	let yAxisG = connectAxisToParent(gWrapper, `translate(0, 0)`, 'yAxisG');
+		yAxisG.call(yAxisObj)
+
+
+
+
+
+
+
+
+
 
 })
