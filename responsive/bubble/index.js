0d717d0be296bbbca37197751ba7efd3f3e6031b
Eric Laursen <ericjlaursen@gmail.com>
2018-09-11 18:27:16 -0400

    Removes defer, includes hard-coded data array.

--- responsive/bubble/index.js
+++ responsive/bubble/index.js
@@ -3,52 +3,100 @@ let myVars = {
         svgH : 500
       };
 
+let thisDataObj = [
+   {
+   "id": "SteelyDan",
+   "name": "SteelyDan",
+   "sales": 40,
+   "decade":"pre"
+},
+{
+   "id": "Beyonce",
+   "name": "Beyonce",
+   "sales": 34,
+   "decade":"post"
+},
+{
+   "id": "Madonna",
+   "name": "Madonna",
+   "sales": 300,
+   "decade":"pre"
+},
+{
+   "id": "AdamLevine",
+   "name": "AdamLevine",
+   "sales": 20,
+   "decade":"post"
+},
+{
+   "id": "Hall&Oats",
+   "name": "Hall&Oats",
+   "sales": 125,
+   "decade":"pre"
+},
+{
+   "id": "NickiMinaj",
+   "name": "NickiMinaj",
+   "sales": 80,
+   "decade":"post"
+},
+{
+   "id": "Drake",
+   "name": "Drake",
+   "sales": 10,
+   "decade":"post"
+},
+{
+   "id": "Bieber",
+   "name": "Bieber",
+   "sales": 25,
+   "decade":"post"
+}
+]
 
-      let svgObj = d3.select('#chart')
-        .append('svg')
-        .attrs({
-          'width':myVars.svgW,
-          'height':myVars.svgH
-        })
-        .append('g')
-        .attr('transform', "translate(" + myVars.svgW / 2 + "," + myVars.svgH / 2 + ")");
+let svgObj = d3.select('#chart')
+  .append('svg')
+  .attrs({
+    'width':myVars.svgW,
+    'height':myVars.svgH,
+    'class': 'svgWrapper'
+  });
+
+  let gWrapper = svgObj.append('g')
+  .attr('transform', "translate(" + myVars.svgW / 2 + "," + myVars.svgH / 2 + ")")
+  .attr('class', 'gWrapper');
 //RESETS 
 //svg translate to middle
-      let readyFn = (error, datapoints) => {
-        console.log('error is ',error);
-        console.log('datapoints',datapoints);
 
-        let radiusScale = d3.scaleSqrt()
-          .domain(d3.extent(datapoints, (d) => {return +d.sales})) //input values min/max
-          .range([15,100])                                          //output values max/min
-//RESETS
+
+let radiusScale = d3.scaleSqrt();
 //forceY & forceX to default 0
-        let simulation = d3.forceSimulation()
-          .force("yforce", d3.forceY().strength(.1))
-          .force("xforce", d3.forceX().strength(.1))
-          .force("myCollide", d3.forceCollide((d) => { return radiusScale(d.sales)}));
+let simulation = d3.forceSimulation()
+  .force("yforce", d3.forceY().strength(.1))
+  .force("xforce", d3.forceX().strength(.1));
 
-        let circlesObj = svgObj.selectAll('.artists')
-            .data(datapoints)
-            .enter()
-            .append('circle')
-            .attrs({
-              'class' : 'artist-circle',
-              'r'     : (d) => { return radiusScale(d.sales)},
-              'fill'  : 'lightblue'
-            });
+  radiusScale
+    .domain(d3.extent(thisDataObj, (d) => {return +d.sales}))
+    .range([15,100])
 
-        let myTickFn = () => {
-            circlesObj.attrs({
-              "cx" : (d) => {return d.x},
-              "cy" : (d) => {return d.y}
-            })
-          }
-        
-        simulation.nodes(datapoints)
-          .on('tick', myTickFn)
-      }
+  simulation.force("myCollide", d3.forceCollide((d) => { return radiusScale(d.sales)}));
 
-      d3.queue()
-        .defer(d3.json, "data.json")
-        .await(readyFn)
+let circlesObj = gWrapper.selectAll('.artists')
+    .data(thisDataObj)
+    .enter()
+    .append('circle')
+    .attrs({
+      'class' : d => `artist-circle ${d.name}`,
+      'r'     : d => radiusScale(d.sales),
+      'fill'  : 'lightblue'
+    });
+
+let myTickFn = () => {
+  circlesObj.attrs({
+    "cx" : (d) => {return d.x},
+    "cy" : (d) => {return d.y}
+  })
+}
+
+simulation.nodes(thisDataObj)
+  .on('tick', myTickFn)
