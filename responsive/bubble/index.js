a3f095d0b08615dcc667782687f5501ab172608d
Eric Laursen <ericjlaursen@gmail.com>
2018-09-12 06:31:23 -0400

    Resizes with window-resize.

--- responsive/bubble/index.js
+++ responsive/bubble/index.js
@@ -21,12 +21,12 @@ function getClientDims(parentDiv, marginObj){
   return { cssDivWidth, cssDivHeight, divWidthLessMargins, divHeightLessMargins };
 }
 
-const margin = { 
-  left: 20, 
-  right: 250,
-  top: 40,
-  bottom: 40
-};
+function setSVGDims(obj, w, h){
+  obj.attrs({
+    "width" : w,
+    "height" : h
+  });
+}
 
 let thisDataObj = [
    {
@@ -34,52 +34,58 @@ let thisDataObj = [
    "name": "SteelyDan",
    "sales": 40,
    "decade":"pre"
-},
-{
-   "id": "Beyonce",
-   "name": "Beyonce",
-   "sales": 34,
-   "decade":"post"
-},
-{
-   "id": "Madonna",
-   "name": "Madonna",
-   "sales": 300,
-   "decade":"pre"
-},
-{
-   "id": "AdamLevine",
-   "name": "AdamLevine",
-   "sales": 20,
-   "decade":"post"
-},
-{
-   "id": "Hall&Oats",
-   "name": "Hall&Oats",
-   "sales": 125,
-   "decade":"pre"
-},
-{
-   "id": "NickiMinaj",
-   "name": "NickiMinaj",
-   "sales": 80,
-   "decade":"post"
-},
-{
-   "id": "Drake",
-   "name": "Drake",
-   "sales": 10,
-   "decade":"post"
-},
-{
-   "id": "Bieber",
-   "name": "Bieber",
-   "sales": 25,
-   "decade":"post"
-}
+  },
+  {
+     "id": "Beyonce",
+     "name": "Beyonce",
+     "sales": 34,
+     "decade":"post"
+  },
+  {
+     "id": "Madonna",
+     "name": "Madonna",
+     "sales": 300,
+     "decade":"pre"
+  },
+  {
+     "id": "AdamLevine",
+     "name": "AdamLevine",
+     "sales": 20,
+     "decade":"post"
+  },
+  {
+     "id": "Hall&Oats",
+     "name": "Hall&Oats",
+     "sales": 125,
+     "decade":"pre"
+  },
+  {
+     "id": "NickiMinaj",
+     "name": "NickiMinaj",
+     "sales": 80,
+     "decade":"post"
+  },
+  {
+     "id": "Drake",
+     "name": "Drake",
+     "sales": 10,
+     "decade":"post"
+  },
+  {
+     "id": "Bieber",
+     "name": "Bieber",
+     "sales": 25,
+     "decade":"post"
+  }
 ];
 
 
+const margin = { 
+  left: 20, 
+  right: 250,
+  top: 40,
+  bottom: 40
+};
 const colorScale = d3.scaleOrdinal(d3.schemeCategory20);
 let radiusScale = d3.scaleSqrt();
 //forceY & forceX to default 0
@@ -87,12 +93,13 @@ let simulation = d3.forceSimulation()
   .force("yforce", d3.forceY().strength(.03))
   .force("xforce", d3.forceX().strength(.03));
 
-function buildChart(dataObj){
+const {chartDiv, svgObj, gWrapper} = makeD3ElementsFromParentDiv('chart');
 
-  const {chartDiv, svgObj, gWrapper} = makeD3ElementsFromParentDiv('chart');
+function buildChart(dataObj){
 
   let { cssDivWidth, cssDivHeight, divWidthLessMargins, divHeightLessMargins } = getClientDims(chartDiv, margin);
 
+  let smallestViewSize = Math.min(cssDivHeight, cssDivWidth)
   svgObj.attrs({
     'width':cssDivWidth,
     'height':cssDivHeight,
@@ -104,7 +111,7 @@ function buildChart(dataObj){
 
   radiusScale
     .domain(d3.extent(dataObj, (d) => {return +d.sales}))
-    .range([15,(cssDivWidth/4)])
+    .range([0,(smallestViewSize/4)])
 
   simulation.force("myCollide", d3.forceCollide((d) => { return radiusScale(d.sales)}));
 
@@ -129,4 +136,37 @@ function buildChart(dataObj){
     .on('tick', myTickFn)
 }
 
+function updateData(){
+    console.log('resizing!')
+    let { cssDivWidth, cssDivHeight, divWidthLessMargins, divHeightLessMargins } = getClientDims(chartDiv, margin)
+    setSVGDims(svgObj, cssDivWidth, cssDivHeight);
+    gWrapper.attr('transform', `translate(${cssDivWidth / 2},${cssDivHeight / 2 })`)
+    let smallestResizeVal = Math.min(cssDivHeight, cssDivWidth)
+    console.log(smallestResizeVal)
+
+    radiusScale.range([0,(smallestResizeVal/4)])
+
+    simulation
+      .force("myCollide", d3.forceCollide((d) => { return radiusScale(d.sales)}))
+      .alpha(.5)
+      .restart();
+
+
+  let resizeCirclesObj = gWrapper.selectAll('.artist-circle')
+    .attr('r', d => radiusScale(d.sales));
+
+    let resizeTick = () => {
+      resizeCirclesObj.attrs({
+        "cx" : (d) => {return d.x},
+        "cy" : (d) => {return d.y}
+      })
+    }
+
+    simulation.nodes(thisDataObj)
+      .on('tick', resizeTick)
+}
+
 buildChart(thisDataObj)
+
+//Resise listener & fn call
+d3.select(window).on('resize',updateData);
