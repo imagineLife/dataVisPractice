7531784a8b9f01f3a50ff5b6738adfbbccf117d6
Eric Laursen <ericjlaursen@gmail.com>
2018-10-04 16:46:26 -0400

    initial commit.

---
+++ worldMapFun/3.differentGeoPackage/index.js
@@ -0,0 +1,27 @@
+const svgObj = d3.select('.svgWrapper');
+
+//swap these in the .projection-passer in pathGenerator
+//checkout d3-map-projection for EVEN MORE projections
+const geoNatural = d3.geoNaturalEarth1();
+const geoOrth = d3.geoOrthographic();
+const geoStereo = d3.geoStereographic();
+const geoEquiRect = d3.geoEquirectangular();
+
+
+const pathGenerator = d3.geoPath().projection(geoNatural);
+
+
+
+d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json').then(data => {
+	const countries = topojson.feature(data, data.objects.countries);
+	
+	//data-join for countries
+	const countryPaths = svgObj.selectAll('path')
+		.data(countries.features);
+
+	//append a path for each country
+	countryPaths.enter().append('path')
+	//set d based on country
+	.attr('d', d => pathGenerator(d))
+	.attr('class','countryPath');
+})
