9e26f63a9275385e2dadc3741ef252ded4df4b47
Eric Laursen <ericjlaursen@gmail.com>
2018-10-16 14:15:15 -0400

    Adds text to parent g wrappers.

---
+++ enterUpdateExit/processOverview/11.9.3MakeTextNested/drawChart.js
@@ -0,0 +1,44 @@
+//32:00
+const colorScale = d3.scaleOrdinal()
+.domain(['apple','lemon']).range(['darkred','khaki']);
+
+const radScale = d3.scaleOrdinal()
+.domain(['apple','lemon']).range([50,30])
+
+const drawChart = (parent, { fruits }) => {
+	// const { fruits } = props;
+
+	const groupDataJoin = parent.selectAll('g')
+		.data(fruits);
+
+	const groupEnterDataJoin = groupDataJoin.enter().append('g');
+		groupEnterDataJoin.merge(groupDataJoin)
+		.attrs({
+			'transform': (d,i) => `translate(${((i * 120) + 60)},${(h/2)})`,
+			'class': `appleTextGWrapper`
+		})
+
+	//'groupDataJoin' returns the same selection selection
+	groupDataJoin.exit().remove();
+
+//fruitCircles appended to g 
+	const fruitDataJoin = groupDataJoin.select('circle');
+
+	groupEnterDataJoin.append('circle')
+		.merge(fruitDataJoin)
+		.attrs({
+			'r' : d => radScale(d.type),
+			'fill' : d => colorScale(d.type)
+		})
+
+	//ADDING LABELS
+//LABELS appended to g 
+	const labelDataJoin = groupDataJoin.select('text');
+
+	groupEnterDataJoin.append('text')
+		.merge(labelDataJoin)
+		.attr('y', 120)
+		.text(d => d.type)
+
+		
+}
