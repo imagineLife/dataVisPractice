4d95c17488a06bad6200c0cad5b6febdc207ce1f
Eric Laursen <ericjlaursen@gmail.com>
2018-11-02 12:52:53 -0400

    converts map to individual fn.

--- worldMapFun/10/index.js
+++ worldMapFun/10/index.js
@@ -1,14 +1,5 @@
 function buildChart(){
 
-	//new path
-	gObj.append('path')
-		.attr('d', pathGenerator({type: 'Sphere'}))
-		.attr('class', 'globeSpherePath');
-
-	svgObj.call(d3.zoom().on('zoom', function(){
-		gObj.attr("transform", d3.event.transform);
-	}));
-
 	let mappedColors = stateCountryFeats.map(d => {
 		return d.properties.economy
 	})
@@ -26,23 +17,13 @@ function buildChart(){
 		selectedLegend,
 		selectedLegendVal,
 	});
-	
-	//data-join for countries to paths
-	const countryPaths = gObj.selectAll('path')
-		.data(stateCountryFeats);
 
-	//append a path for each country
-	countryPaths.enter().append('path')
-	.attrs({
-		'd': d => pathGenerator(d), //set d based on country
-		'class':'countryPath',
-		// 'fill': d => colorScale(d.properties.name) //Rainbow
-		'fill': d => colorScale(colorVal(d))
+	mapG.call(buildMap, {
+		stateCountryFeats,
+		colorScale,
+		colorVal	
 	})
-	//append the title for mouseover 'tooltip'
-	.append('title')
-	.text(d => `${d.properties.name}: ${d.properties.economy}`);
-
+	
 
 }
 
@@ -51,13 +32,18 @@ const svgObj = d3.select('.svgWrapper');
 
 //swap these in the .projection-passer in pathGenerator
 //checkout d3-map-projection for EVEN MORE projections
-const geoNatural = d3.geoNaturalEarth1();
+
+//MOVED to buildMap
+// const geoNatural = d3.geoNaturalEarth1();
+// const pathGenerator = d3.geoPath().projection(geoNatural);
+
+
 //this one is the globe!
 const geoOrth = d3.geoOrthographic();
 const geoStereo = d3.geoStereographic();
 const geoEquiRect = d3.geoEquirectangular();
 const colorScale = d3.scaleOrdinal();
-const pathGenerator = d3.geoPath().projection(geoNatural);
+
 const colorVal = d => d.properties.economy;
 let selectedLegendVal;
 let stateCountryFeats;
@@ -66,9 +52,7 @@ const selectedLegend = (d) => {
 	buildChart()
 }
 
-let gObj = svgObj.append('g').attr('pointer-events', 'all')
-
-
+let mapG = svgObj.append('g').attr('pointer-events', 'all')
 let colorLegendBox = svgObj.append('rect')
 	.attrs({
 		'class': 'legendBox',
@@ -86,6 +70,8 @@ let colorLegendG = svgObj.append('g').attrs({
 	'class': 'colorLegendG',
 	'transform': 'translate(25,325)'
 	});
+
+
 
 
 //run the project
