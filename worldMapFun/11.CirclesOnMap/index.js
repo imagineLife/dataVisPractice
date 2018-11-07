848c17abb27e4ece103d3e001c94ef929c34588c
Eric Laursen <ericjlaursen@gmail.com>
2018-11-07 06:25:40 -0500

    removes color references

--- worldMapFun/11.CirclesOnMap/index.js
+++ worldMapFun/11.CirclesOnMap/index.js
@@ -1,27 +1,7 @@
 function buildChart(){
 
-	let mappedColors = stateCountryFeats.map(d => {
-		return d.properties.economy
-	})
-
-	colorScale
-		.domain(mappedColors)
-		.domain(colorScale.domain().sort().reverse())
-		.range(d3.schemeSpectral[7])
-
-	colorLegendG.call(buildColorLegend, {
-		colorScale,
-		circleRadius: 10,
-		spacing: 25,
-		textOffset: 15,
-		selectedLegend,
-		selectedLegendVal,
-	});
-
 	mapG.call(buildMap, {
 		stateCountryFeats,
-		colorScale,
-		colorVal,
 		selectedLegendVal	
 	})
 	
@@ -43,9 +23,7 @@ const svgObj = d3.select('.svgWrapper');
 const geoOrth = d3.geoOrthographic();
 const geoStereo = d3.geoStereographic();
 const geoEquiRect = d3.geoEquirectangular();
-const colorScale = d3.scaleOrdinal();
 
-const colorVal = d => d.properties.economy;
 let selectedLegendVal;
 let stateCountryFeats;
 const selectedLegend = (d) => {
@@ -54,26 +32,6 @@ const selectedLegend = (d) => {
 }
 
 let mapG = svgObj.append('g').attr('pointer-events', 'all')
-let colorLegendBox = svgObj.append('rect')
-	.attrs({
-		'class': 'legendBox',
-		'x': 5,
-		'y': 300,
-		'width': 264,
-		'height': '195',
-		'fill' : 'rgba(255,255,255,.8)',
-		'rx': 25,
-		'ry': 25
-	})
-
-//color legend g wrapper
-let colorLegendG = svgObj.append('g').attrs({
-	'class': 'colorLegendG',
-	'transform': 'translate(25,325)'
-	});
-
-
-
 
 //run the project
 loadAndProcessData().then(countries => {
