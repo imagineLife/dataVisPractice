a64a9953bf910c3c6baed8ab1b75e75b76a42e90
Eric Laursen <ericjlaursen@gmail.com>
2018-11-12 16:10:21 -0500

    Builds more parts from lib

--- responsive/scatter/index.js
+++ responsive/scatter/index.js
@@ -15,13 +15,10 @@ const margin = {
 	bottom: 120
 };
 
+let {chartDiv, svgObj, gObj} = lib.makeD3ObjsFromParentID('chartDiv')
+
 //Select/Create div, svg, g
-const chartDiv = document.getElementById('chartDiv'); 	      
-const svgObj = d3.select(chartDiv).append("svg").attrs({
-	// "class":obj.svgClass,
-	"border": '2px solid green'
-});
-const gObj = svgObj.append('g').attr('class','gWrapper');
+let { parentDivWidth, parentDivHeight, divWidthLessMargins, divHeightLessMargins } =  lib.getDimsFromParent(chartDiv, margin);
 
 //Setup Scales
 const xScale = d3.scaleTime();
@@ -30,36 +27,20 @@ const colorScale = d3.scaleOrdinal()
 .range(d3.schemeCategory10);
 // const radiusScale = d3.scaleLinear();
 
-
-// Extract the DIV width and height that was computed by CSS.
-let cssDivWidth = chartDiv.clientWidth;
-let cssDivHeight = chartDiv.clientHeight;
-
-//get css-computed dimensions
-const divWidthLessMargins =cssDivWidth - margin.left - margin.right;
-const divHeightLessMargins = cssDivHeight - margin.top - margin.bottom;
-
 //set svg height & width from div computed dimensions
 //NOTE: can be the divLessMargins, for 'padding' effect
 svgObj.attrs({
-"width" : cssDivWidth,
-"height" : cssDivHeight
+	"width" : parentDivWidth,
+	"height" : parentDivHeight
 });
 
 //translate the gWrapper
 gObj.attr('transform', `translate(${margin.left},${margin.top})`);
 
 //Build Axis Groups
-const xAxisG = gObj.append('g')
-  .attrs({
-  	'transform': `translate(0, ${divHeightLessMargins})`,
-  	'class':'axis x'
-  });
-const yAxisG = gObj.append('g')
-	.attrs({
-		'class': 'axis y',
-		// 'transform': `rotate(-90)`
-	});
+const xAxisG = lib.makeAxisG(gObj, `translate(0, ${divHeightLessMargins})`, 'axis x');
+const yAxisG = lib.makeAxisG(gObj, ``, 'axis y');
+
 const colorLegendG = gObj.append('g')
   .attr('transform', `translate(${divWidthLessMargins + 60}, 150)`);
 
@@ -69,23 +50,8 @@ let xAxisLabel = xAxisG.append('text');
 let yAxisLabel = yAxisG.append('text');
 
 //set attrs for axis labels      
-xAxisLabel
-  .attrs({
-  	'class': 'axis-label',
-	'x': (divWidthLessMargins / 2),
-  	'y': '100'
-  })
-  .text(xLabel);
-
-yAxisLabel
-  .attrs({
-  	'class': 'axis-label',
-    'x' : -divHeightLessMargins / 2,
-    'y' : -margin.left / 1.5,
-    'transform' : `rotate(-90)`
-  })
-  .style('text-anchor', 'middle')
-  .text(yLabel);
+xAxisLabel = lib.makeAxisLabel(xAxisG, 'axis-label', (divWidthLessMargins / 2), 100, '', '', xLabel);
+yAxisLabel = lib.makeAxisLabel(yAxisG, 'axis-label', (-divHeightLessMargins / 2), (-margin.left / 1.5), `rotate(-90)`, 'middle', yLabel);
 
 colorLegendG.append('text')
   .attrs({
@@ -105,7 +71,8 @@ const yAxis = d3.axisLeft()
 .scale(yScale)
 .ticks(5)
 .tickPadding(15)
-.tickSize(-divWidthLessMargins);
+.tickSize(-divWidthLessMargins)
+.tickFormat(d3.format(".2s"));
 
 // const colorLegend = d3.legendColor()
 //   .scale(colorScale)
@@ -126,52 +93,52 @@ const parseData = d => {
 
 function buildChart(obj){
 
-d3.csv(obj.dataFile, parseData, data => {
-xScale
-  .domain(d3.extent(data, xValue))
-  .range([0, divWidthLessMargins]);
+	d3.csv(obj.dataFile, parseData, data => {
+		xScale
+		  .domain(d3.extent(data, xValue))
+		  .range([0, divWidthLessMargins]);
 
-yScale
-  .domain(d3.extent(data, yValue))
-  .range([divHeightLessMargins, margin.top])
-  .nice();
+		yScale
+		  .domain(d3.extent(data, yValue))
+		  .range([divHeightLessMargins, margin.top])
+		  .nice();
 
-gObj.selectAll('circle').data(data)
-  .enter().append('circle')
-    .attrs({
-    	'cx': d => xScale(xValue(d)),
-    	'cy': d => yScale(yValue(d)),
-    	'fill': d => colorScale(colorValue(d)),
-    	'fill-opacity': 0.3,
-    	'r': 17,
-    	'class':'circle'
-    });
+		gObj.selectAll('circle').data(data)
+		  .enter().append('circle')
+		    .attrs({
+		    	'cx': d => xScale(xValue(d)),
+		    	'cy': d => yScale(yValue(d)),
+		    	'fill': d => colorScale(colorValue(d)),
+		    	'fill-opacity': 0.3,
+		    	'r': 17,
+		    	'class':'circle'
+		    });
 
-xAxisG.call(xAxis)
-	.selectAll('.tick line').attrs({
-		'class':'xLine',
-		'stroke-dasharray': '1, 5'
+		xAxisG.call(xAxis)
+			.selectAll('.tick line').attrs({
+				'class':'xLine',
+				'stroke-dasharray': '1, 5'
+			});
+
+		xAxisG.selectAll('.tick text')
+		  .attrs({
+		    'transform': 'rotate(-45)',
+		    'text-anchor': 'end',
+		    'alignment-baseline':'middle',
+		    'x': -5,
+		    'y': 15,
+		    'dy':0
+		  })
+
+		yAxisG.call(yAxis)
+			.selectAll('.tick line').attrs({
+				'class':'yLine',
+				'stroke-dasharray': '1, 5'
+			});
+	// colorLegendG.call(colorLegend)
+	//   .selectAll('.cell text')
+	//     .attr('dy', '0.1em');
 	});
-
-xAxisG.selectAll('.tick text')
-  .attrs({
-    'transform': 'rotate(-45)',
-    'text-anchor': 'end',
-    'alignment-baseline':'middle',
-    'x': -5,
-    'y': 15,
-    'dy':0
-  })
-
-yAxisG.call(yAxis)
-	.selectAll('.tick line').attrs({
-		'class':'yLine',
-		'stroke-dasharray': '1, 5'
-	});
-// colorLegendG.call(colorLegend)
-//   .selectAll('.cell text')
-//     .attr('dy', '0.1em');
-});
 
 }
 
@@ -184,72 +151,38 @@ buildChart(AllChartObj);
 //2. Build fn
 let resize = () => {
 
-// Extract the width and height that was computed by CSS.
-	let resizedFnWidth = chartDiv.clientWidth;
-	let resizedFnHeight = chartDiv.clientHeight;
+	//Select/Create div, svg, g
+	let { parentDivWidth, parentDivHeight, divWidthLessMargins, divHeightLessMargins } =  lib.getDimsFromParent(chartDiv, margin);
 
 	//set svg dimension based on resizing attrs
 	svgObj.attrs({
-    "width" : resizedFnWidth,
-    "height" : resizedFnHeight
-});
-
-//calc resized dimensions less margins
-let resizedWidthLessMargins = resizedFnWidth - margin.left - margin.right;
-	let resizedHeightLessMargins = resizedFnHeight - margin.top - margin.bottom;
+	    "width" : parentDivWidth,
+	    "height" : parentDivHeight
+	});
 
 	//update scale ranges
-	xScale.range([0, resizedWidthLessMargins]);
-	yScale.range([resizedHeightLessMargins, margin.top]);
+	xScale.range([0, divWidthLessMargins]);
+	yScale.range([divHeightLessMargins, margin.top]);
 
-	//Update the X-AXIS
-xAxisG
-	.attrs({
-	    'transform': `translate(0, ${resizedHeightLessMargins})`,
-	    'x' : divWidthLessMargins / 2,
-	    'y' : resizedFnHeight * .1,
-	})
-	.call(xAxis);
+	//Update the X & Y axis
+    lib.resetAxisG(xAxisG, `translate(0, ${divHeightLessMargins})`, (divWidthLessMargins / 2), (divHeightLessMargins * .1), xAxis)
+    lib.resetAxisG(yAxisG, ``, (-divHeightLessMargins / 2), (-margin.left / 2), yAxis)
 
-//Update the X-AXIS LABEL
-xAxisLabel
-	.attrs({
-	  'x' : resizedWidthLessMargins / 2,
-	  // 'y' : resizedFnHeight * .17
-	})
+ //update axis label positions
+    lib.updateAxisLabelXY(xAxisLabel, divWidthLessMargins / 2, 100 )
+    lib.updateAxisLabelXY(yAxisLabel, (-divHeightLessMargins / 2), (-margin.left / 1.5))
 
-//Update the Y-AXIS
-yAxisG
-	.attrs({
-	    'x' : -resizedHeightLessMargins / 2,
-	    'y' : -margin.left / 2,
-	})
-	.call(yAxis);
+	//update Bubbles
+	d3.selectAll('.circle').attrs({
+		'cx': d => xScale(xValue(d)),
+		'cy': d => yScale(yValue(d))
+	});
 
-//Update yAxis Label
-yAxisLabel.attrs({
-	'x' : -resizedHeightLessMargins / 2,
-	'y' : -margin.left / 1.5,
-});
+	d3.selectAll('.yLine').attr('x2', divWidthLessMargins);
+	d3.selectAll('.xLine').attr('y2', -divHeightLessMargins);
 
-//update Bubbles
-d3.selectAll('.circle').attrs({
-	'cx': d => xScale(xValue(d)),
-	'cy': d => yScale(yValue(d))
-});
-
-d3.selectAll('.yLine')
-	.attr('x2', resizedWidthLessMargins);
-
-colorLegendG
-  .attr('transform', `translate(${resizedWidthLessMargins + 60}, 150)`);	
-
-	// yAxis.ticks(Math.max(resizedHeightLessMargins/80, 2))
-	// 	.attrs({
-//   		'class':'yLine',
-//   		'stroke-dasharray': '1, 5'
-//   	});
-
+	// colorLegendG
+	//   .attr('transform', `translate(${divWidthLessMargins + 60}, 150)`);	
 
 }	  	
 
