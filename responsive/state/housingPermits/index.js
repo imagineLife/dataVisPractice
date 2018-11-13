a18add2447286afdd6d5945565ee24aaa0781f58
Eric Laursen <ericjlaursen@gmail.com>
2018-11-13 07:06:44 -0500

    moves chart-building to fn, loads 2 datasets.

--- responsive/state/housingPermits/index.js
+++ responsive/state/housingPermits/index.js
@@ -1,41 +1,19 @@
 function resize(){
     
     let { parentDivWidth, parentDivHeight, divWidthLessMargins, divHeightLessMargins } = lib.getDimsFromParent(chartDiv, margin);
-
-    svgObj.attr("height", parentDivWidth * .95);
-    gObj.attr('transform', `scale(${parentDivWidth/900}) translate(${parentDivWidth * .03 },0)`);
+    
+    let resizeW = (parentDivWidth > 1530) ? 1530 : parentDivWidth
+    svgObj.attr("height", resizeW * .95);
+    gObj.attr('transform', `scale(${resizeW/900}) translate(${parentDivWidth * .03 },0)`);
     d3.selectAll('.statePath').attr('d', d => pathGenerator(d))
 
 }
 
-let removeWater = (d) => d.properties["NAME10"].indexOf('defined') < 0;
-let showTownName = d => d.properties["NAME10"];
-const margin = { 
-    left: 20, 
-    right: 20,
-    top: 20,
-    bottom: 20
-};
-let {chartDiv, svgObj, gObj} = lib.makeD3ObjsFromParentID('chartDiv');
-let { parentDivWidth, parentDivHeight, divWidthLessMargins, divHeightLessMargins } = lib.getDimsFromParent(chartDiv, margin);
-var projection = d3.geoAlbersUsa();
-const pathGenerator = d3.geoPath().projection(projection);
-
-//set svg height & width from div computed dimensions
-//NOTE: can be the divLessMargins, for 'padding' effect
-svgObj.attrs({
-    "width" : '100%',
-    "height" : parentDivWidth * .95
-});
-
-gObj.attr('transform', `scale(${parentDivWidth/900}) translate(${parentDivWidth * .03},0)`);
-
-d3.json('CTstate.json').then(data => {
-
-    let geometriesWithoutWater = data.objects.townLayer.geometries.filter(removeWater)
+function buildChart(permitData, townShapeData){
+    let geometriesWithoutWater = townShapeData.objects.townLayer.geometries.filter(removeWater)
 
     //Connecticut topojson
-    var connecticut = topojson.feature(data, {
+    var connecticut = topojson.feature(townShapeData, {
         type: "GeometryCollection",
         geometries: geometriesWithoutWater
     });
@@ -55,6 +33,40 @@ d3.json('CTstate.json').then(data => {
     .on('click', d => console.log(d.properties["NAME10"]))
     .append('title')
         .text(showTownName);
+}
+
+
+let removeWater = (d) => d.properties["NAME10"].indexOf('defined') < 0;
+let showTownName = d => d.properties["NAME10"];
+const margin = { 
+    left: 20, 
+    right: 20,
+    top: 20,
+    bottom: 20
+};
+let {chartDiv, svgObj, gObj} = lib.makeD3ObjsFromParentID('chartDiv');
+let { parentDivWidth, parentDivHeight, divWidthLessMargins, divHeightLessMargins } = lib.getDimsFromParent(chartDiv, margin);
+var projection = d3.geoAlbersUsa();
+const pathGenerator = d3.geoPath().projection(projection);
+
+//set svg height & width from div computed dimensions
+//NOTE: can be the divLessMargins, for 'padding' effect
+let resizeW = (parentDivWidth > 1530) ? 1530 : parentDivWidth
+svgObj.attrs({
+    "width" : '100%',
+    "height" : resizeW * .95
+});
+
+gObj.attr('transform', `scale(${resizeW/900}) translate(${resizeW * .03},0)`);
+
+let townVals, townShapes;
+d3.csv('./data.csv').then(csvData => {
+    townVals = csvData;
+
+    d3.json('CTstate.json').then(data => {
+        townShapes = data;
+        buildChart(townVals, townShapes);
+    })
 })
 
 //Add Resise listener & fn call
