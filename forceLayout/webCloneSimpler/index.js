30fcd532fb8cebcd343450ebfc40a3bd2af5eba4
Eric Laursen <ericjlaursen@gmail.com>
2018-12-02 20:21:07 -0500

    Renders 2 circles, 1 link mid-screen

--- forceLayout/webCloneSimpler/index.js
+++ forceLayout/webCloneSimpler/index.js
@@ -1,40 +1,66 @@
-function tickFn(){
+function tickFn(d){
   console.log('ticking!')
+  links.attrs({
+    "x1": d => d.source.x,
+    "y1": d => d.source.y,
+    "x2": d => d.target.x,
+    "y2": d => d.target.y
+  });
+
+  nodes
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
 }
 
 function drawChart(chartData){
   console.log('drawingChart!')
   console.log(chartData)
 
-  let linkGWrapper = svgObj.append('g')
-    .attr('class', 'linksGWrapper');
-
-    linkGWrapper.selectAll('line')
+  var link = svgObj.append("g")
+    .attr("class", "links")
+    .selectAll("line")
     .data(chartData.links)
-    .enter()
-      .append('line')
-      .attrs({
-        'class': 'linkLine',
-        'stroke-width': d => Math.sqrt(d.value)
-      });
-
-  let nodeGWrapper = svgObj.append('g')
-    .attr('class', 'nodeGWrapper');
-
-  nodeGWrapper.selectAll('circle')
+    .enter().append("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value));
+  
+  // Add circles for every node in the dataset
+  var node = svgObj.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
     .data(chartData.nodes)
-    .enter()
-      .append('circle')
-      .attrs({
-        'class': 'nodeCircle',
-        'r': 5,
-        'fill': d => colorScale(d.group)
-      })
-
+    .enter().append("circle")
+    .attr("r", 5)
+    .attr("fill", function(d) { return colorScale(d.group); })
+    // .call(d3.drag()
+    //     .on("start", dragstarted)
+    //     .on("drag", dragged)
+    //     .on("end", dragended)
+    // );
+  
+  // Basic tooltips
+  node.append("title")
+    .text(function(d) { return d.id; });
+  
   // Attach nodes to the simulation, add listener on the "tick" event
   forceSim
-      .nodes(chartData.nodes)
-      .on('tick', tickFn)
+    .nodes(chartData.nodes)
+    .on("tick", (d) => ticked(d));
+  
+  // Associate the lines with the "link" force
+  forceSim.force("link")
+    .links(chartData.links)
+  
+  // Dynamically update the position of the nodes/links as time passes
+  function ticked(d) {
+    link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+    node
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  }
 
 
 }
@@ -44,22 +70,23 @@ svgWidth = +svgObj.attr('width'),
 svgHeight = +svgObj.attr('height'),
 colorScale = d3.scaleOrdinal(d3.schemeCategory20);
 
-let forceSim = d3.forceSimulation()
-
-  //moves to center of screen
-  .force('center', d3.forceCenter(svgWidth/2, svgHeight / 2))
-
-  //
-  .force('charge', d3.forceManyBody().strength(-50))
-
-  //
-  .force('collide', d3.forceCollide(10).strength(.9))
-
-  //
-  .force('link', d3.forceLink().id(d => d.id))
-
-d3.json('./data.json', (err, data) => {
-  if(err) throw err;
-
-  drawChart(data)
-})
+// Add "forces" to the simulation here
+    var forceSim = d3.forceSimulation()
+        .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2))
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("collide", d3.forceCollide(10).strength(0.9))
+        .force("link", d3.forceLink().id(function(d) { return d.id; }));
+    
+    d3.json("./data.json", function(error, graph) {
+        if (error) throw error;
+        console.log(graph);
+        // Add lines for every link in the dataset
+        drawChart(graph)
+        
+    });
+    // Change the value of alpha, so things move around when we drag a node
+    function dragstarted(d) {
+    if (!d3.event.active) forceSim.alphaTarget(0.7).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
