14492c5e298fddd05ff94b9dfdefdf6705524408
Eric Laursen <ericjlaursen@gmail.com>
2018-12-03 20:48:17 -0500

    adds comments, updates code layout

--- treeLayout/fitToScreen/index.js
+++ treeLayout/fitToScreen/index.js
@@ -2,8 +2,6 @@ let getParentFromD = (d) => d.id.substring(0, d.id.lastIndexOf("."));
 
 let mySort = (a, b) => a.height - b.height;
 
-let setText = (d) => d.id.substring(d.id.lastIndexOf(".") + 1);
-
 function makeObjsFromParent(parent){
   let chartObj = d3.select('#chartDiv'),
   svgObj = chartObj.append('svg'),
@@ -14,18 +12,21 @@ function makeObjsFromParent(parent){
 }
 
 function prepData(data){
-    //stratify data
+  
+  //stratify data
   var stratRootData = d3.stratify()
     .id(d => d.name)
     .parentId(d => d.parent)
     (data);
 
+  //build nodes using hierarchy
   var nodes = d3.hierarchy(data, d => d.children);
   return {stratRootData, nodes};
 }
 
 function getDimsFromParent(p){
-    // Extract the width and height that was computed by CSS.
+  
+  // Extract the width and height that was computed by CSS.
   let resizedWidth = p.clientWidth;
   let resizedHeight = p.clientHeight;
   const widthLessMargins = resizedWidth - margin.left - margin.right;
@@ -38,14 +39,17 @@ function resize(){
 
   let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
 
+  //set svg dims
   svgObj.attrs({
     'height': heightLessMargins,
     'width': widthLessMargins,
-    'transform': `translate(${margin.left},0)`
+    'transform': `translate(${margin.left},${margin.top})`
   })
 
+  //transform gObj
   gObj.attr('transform', `translate(${75},${margin.top})`)
 
+  //reset three dims
   tree.size([heightLessMargins, widthLessMargins - 200])
 
   buildChart(rootData, storedNodes)
@@ -57,6 +61,7 @@ function buildChart(stratRootData, nodes){
   var linkDataJoin = gObj.selectAll(".link")
     .data(tree(stratRootData).links());
 
+    //set link paths & link path data
     linkDataJoin
       .enter().append("path")
         .attr("class", "link")
@@ -65,19 +70,22 @@ function buildChart(stratRootData, nodes){
           "d", d3.linkHorizontal().x(d => d.y).y(d => d.x)
         );
 
-  // Add groups for each node in the hierarchy with circles and text labels
+  // Set node DataJoin
   var nodeDataJoin = gObj.selectAll(".node")
     .data(stratRootData.descendants());
 
+  //build nodeEnter for appending cnode circle & text
   let nodeEnter = nodeDataJoin.enter().append("g")
-  .merge(nodeDataJoin)
+    .merge(nodeDataJoin)
     .attrs({
       "class": d => "node" + (d.children ? " node--mid" : " node--final"),
       "transform": d => `translate(${d.y},${d.x})`
     })
 
+  //build circles
   let nodeCircle = nodeEnter.append("circle").attr("r", 2.5);
 
+  //build node Text
   let nodeTxt = nodeEnter.append("text")
     .attrs({
       "dy": 3,
@@ -86,10 +94,10 @@ function buildChart(stratRootData, nodes){
       'class':'nodeText'
     })
     .style("text-anchor", d => d.children ? "end" : "start")
-    .text(setText);
+    .text((d) => d.id);
 }
 
-const margin = { left: 20, right: 20, top: 20, bottom: 0 };
+const margin = { left: 20, right: 20, top: 20, bottom: 20 };
 let rootData, storedNodes;
 let {chartObj, svgObj, svgW, svgH, gObj} = makeObjsFromParent('chartDiv');  
 let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
@@ -97,24 +105,25 @@ let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDims
 svgObj.attrs({
   'height': heightLessMargins,
   'width': widthLessMargins,
-  'transform': `translate(${margin.left},0)`
+  'transform': `translate(${margin.left},${margin.top})`
 })
 
 gObj.attr('transform', `translate(${75},${margin.top})`)
 
 let tree = d3.tree()
-    .size([heightLessMargins, widthLessMargins - 200]);
+  .size([heightLessMargins, widthLessMargins - 200]);
 
 var stratify = d3.stratify()
-    .parentId(getParentFromD);
+  .parentId(getParentFromD);
 
 d3.json("./data.json", function(error, data) {
-    if (error) throw error;
+  if (error) throw error;
 
-    let {stratRootData, nodes} = prepData(data)
-    rootData = stratRootData;
-    storedNodes = nodes;
-    buildChart(stratRootData, nodes);
+  let {stratRootData, nodes} = prepData(data);
+  
+  rootData = stratRootData;
+  storedNodes = nodes;
+  buildChart(stratRootData, nodes);
 });
 
 // Call the resize function whenever a resize event occurs
