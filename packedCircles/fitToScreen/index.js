2963e7cb9f2c82dc284bdf85ff9fca90c1e1c12f
Eric Laursen <ericjlaursen@gmail.com>
2018-12-11 18:24:37 -0500

    fits to screen on load

--- packedCircles/fitToScreen/index.js
+++ packedCircles/fitToScreen/index.js
@@ -1,25 +1,9 @@
-let getParentFromD = (d) => d.id.substring(0, d.id.lastIndexOf("."));
-
 function makeObjsFromParent(parent){
   let chartObj = d3.select('#chartDiv'),
   svgObj = chartObj.append('svg'),
   svgW = +svgObj.attr("width"),
-  svgH = +svgObj.attr("height"),
-  gObj = svgObj.append("g");
-  return {chartObj, svgObj, svgW, svgH, gObj}
-}
-
-function prepData(data){
-  
-  //stratify data
-  var stratRootData = d3.stratify()
-    .id(d => d.name)
-    .parentId(d => d.parent)
-    (data);
-
-  //build nodes using hierarchy
-  var nodes = d3.hierarchy(data, d => d.children);
-  return {stratRootData, nodes};
+  svgH = +svgObj.attr("height");
+  return {chartObj, svgObj, svgW, svgH}
 }
 
 function getDimsFromParent(p){
@@ -32,72 +16,14 @@ function getDimsFromParent(p){
   return {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins}
 }
 
-function resize(){
-  gObj.selectAll('*').remove()
+let sortFn = (a,b) => b.value - a.value;
+let valFn = d => d.value;
+let nodeFn = d => d.thisNode;
+let allButLastSegment = d => d.id.substring(0, d.id.lastIndexOf("."));
 
-  let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
-
-  //set svg dims
-  svgObj.attrs({
-    'height': heightLessMargins,
-    'width': widthLessMargins,
-    'transform': `translate(${margin.left},${margin.top})`
-  })
-
-  //transform gObj
-  gObj.attr('transform', `translate(${75},${margin.top})`)
-
-  //reset three dims
-  tree.size([heightLessMargins, widthLessMargins - 200])
-
-  buildChart(rootData, storedNodes)
-}
-
-function buildChart(stratRootData, nodes){
-
-  // Add the links (given by calling tree(root), which also adds positional x/y coordinates) for the nodes
-  var linkDataJoin = gObj.selectAll(".link")
-    .data(tree(stratRootData).links());
-
-    //set link paths & link path data
-    linkDataJoin
-      .enter().append("path")
-        .attr("class", "link")
-      .merge(linkDataJoin)
-        .attr(
-          "d", d3.linkHorizontal().x(d => d.y).y(d => d.x)
-        );
-
-  // Set node DataJoin
-  var nodeDataJoin = gObj.selectAll(".node")
-    .data(stratRootData.descendants());
-
-  //build nodeEnter for appending cnode circle & text
-  let nodeEnter = nodeDataJoin.enter().append("g")
-    .merge(nodeDataJoin)
-    .attrs({
-      "class": d => "node" + (d.children ? " node--mid" : " node--final"),
-      "transform": d => `translate(${d.y},${d.x})`
-    })
-
-  //build circles
-  let nodeCircle = nodeEnter.append("circle").attr("r", 2.5);
-
-  //build node Text
-  let nodeTxt = nodeEnter.append("text")
-    .attrs({
-      "dy": 3,
-      "x": d => d.children ? -8 : 8,
-      "y": d => (d.children && d.parent !== null) ? -8 : 0,
-      'class':'nodeText'
-    })
-    .style("text-anchor", d => d.children ? "end" : "start")
-    .text((d) => d.id);
-}
-
-const margin = { left: 20, right: 20, top: 20, bottom: 20 };
+const margin = { left: 20, right: 20, top: 40, bottom: 40 };
 let rootData, storedNodes;
-let {chartObj, svgObj, svgW, svgH, gObj} = makeObjsFromParent('chartDiv');  
+let {chartObj, svgObj, svgW, svgH} = makeObjsFromParent('chartDiv');  
 let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
 
 svgObj.attrs({
@@ -106,23 +32,80 @@ svgObj.attrs({
   'transform': `translate(${margin.left},${margin.top})`
 })
 
-gObj.attr('transform', `translate(${75},${margin.top})`)
+console.log('resizedWidth')
+console.log(resizedWidth)
 
-let tree = d3.tree()
-  .size([heightLessMargins, widthLessMargins - 200]);
 
-var stratify = d3.stratify()
-  .parentId(getParentFromD);
+var format = d3.format(",d");
 
-d3.json("./data.json", function(error, data) {
+var color = d3.scaleSequential(d3.interpolateMagma)
+    .domain([-4, 4]);
+
+var stratify = d3.stratify().parentId(allButLastSegment);
+
+var pack = d3.pack()
+    .size([widthLessMargins - 2, heightLessMargins - 2])
+    .padding(3);
+
+let makeRoot = (data) => {
+  return stratify(data)
+    .sum(valFn)
+    .sort(sortFn);
+}
+
+let hovered = (hover) => {
+  return d => {
+    d3.selectAll(d.ancestors().map(nodeFn)
+    ).classed("node--hover", hover);
+  }
+}
+
+d3.csv("data.csv", (error, data) => {
   if (error) throw error;
 
-  let {stratRootData, nodes} = prepData(data);
-  
-  rootData = stratRootData;
-  storedNodes = nodes;
-  buildChart(stratRootData, nodes);
-});
+  var root = makeRoot(data);
 
-// Call the resize function whenever a resize event occurs
-d3.select(window).on('resize', resize);
+  pack(root);
+
+  var node = svgObj.selectAll("g")
+    .data(root.descendants())
+    .enter().append("g")
+      .attrs({
+        "transform": d => `translate(${d.x},${d.y})`,
+        "class": d => "nodeGWrapper node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root")
+      })
+      .each(function(d) {
+        // console.log('each node g THIS') 
+        // console.log(this)
+        return d.thisNode = this })
+      .on("mouseover", hovered(true))
+      .on("mouseout", hovered(false));
+
+  node.append("circle")
+    .attrs({
+      "id": d => "node-" + d.id,
+      "r": d => d.r
+    })
+    .style("fill", d => color(d.depth))
+
+  var childlessNodes = node.filter(d => !d.children);
+
+  childlessNodes.append("clipPath")
+      .attr("id", d => "clip-" + d.id)
+    .append("use")
+      .attr("xlink:href", d => `#node-${d.id}`);
+
+  childlessNodes.append("text")
+      .attr("clip-path", d => `url(#clip-${d.id})`)
+    .selectAll("tspan")
+    .data(d => d.id.substring(d.id.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g))
+    .enter().append("tspan")
+      .attrs({
+        "x": 0,
+        "y": (d, i, nodes) => 13 + (i - nodes.length / 2 - 0.5) * 10
+      })
+      .text(d => d);
+
+  node.append("title")
+      .text(d => d.id + "\n" + format(d.value));
+});
--- packedCircles/index.js
+++ packedCircles/index.js
@@ -1,3 +1,8 @@
+let sortFn = (a,b) => b.value - a.value;
+let valFn = d => d.value;
+let nodeFn = d => d.node;
+let allButLastSegment = d => d.id.substring(0, d.id.lastIndexOf("."));
+
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
@@ -7,21 +12,18 @@ var format = d3.format(",d");
 var color = d3.scaleSequential(d3.interpolateMagma)
     .domain([-4, 4]);
 
-var stratify = d3.stratify()
-    .parentId(d => d.id.substring(0, d.id.lastIndexOf(".")));
+var stratify = d3.stratify().parentId(allButLastSegment);
 
 var pack = d3.pack()
     .size([width - 2, height - 2])
     .padding(3);
 
-let sortFn = (a,b) => b.value - a.value;
-let valFn = d => d.value;
-let nodeFn = d => d.node;
 let makeRoot = (data) => {
   return stratify(data)
     .sum(valFn)
     .sort(sortFn);
 }
+
 let hovered = (hover) => {
   return d => {
     d3.selectAll(d.ancestors().map(nodeFn)
