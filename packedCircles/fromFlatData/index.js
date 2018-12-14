c4ba779c1cc7c7bbefcc0e6a4bc5702756dbcfc7
Eric Laursen <ericjlaursen@gmail.com>
2018-12-13 21:01:57 -0500

    working on flatData

--- packedCircles/fromFlatData/index.js
+++ packedCircles/fromFlatData/index.js
@@ -1,67 +1,164 @@
 function makeObjsFromParent(parent){
-  let svgObj = d3.select(parent),
+  let chartObj = d3.select('#chartDiv'),
+  svgObj = chartObj.append('svg'),
   svgW = +svgObj.attr("width"),
-  svgH = +svgObj.attr("height"),
-  gObj = svgObj.append("g").attr("transform", "translate(40,0)");
-  return {svgObj, svgW, svgH, gObj}
+  svgH = +svgObj.attr("height");
+  return {chartObj, svgObj, svgW, svgH}
 }
 
-let getParentFromD = (d) => d.id.substring(0, d.id.lastIndexOf("."));
+function getDimsFromParent(p){
+  
+  // Extract the width and height that was computed by CSS.
+  let resizedWidth = p.clientWidth;
+  let resizedHeight = p.clientHeight;
+  const widthLessMargins = resizedWidth - margin.left - margin.right;
+  const heightLessMargins = resizedHeight - margin.top - margin.bottom;
+  return {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins}
+}
 
-let mySort = (a, b) => a.height - b.height;
+let sortFn = (a,b) => b.value - a.value;
+let valFn = d => d.size;
+let nodeFn = d => d.thisNode;
+let allButLastSegment = d => d.parent//d.id.substring(0, d.id.lastIndexOf("."));
 
-let setText = (d) => d.id.substring(d.id.lastIndexOf(".") + 1);
+const margin = { left: 20, right: 20, top: 40, bottom: 40 };
+let rootData, storedNodes;
+let {chartObj, svgObj, svgW, svgH} = makeObjsFromParent('chartDiv');  
+let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
+
+svgObj.attrs({
+  'height': heightLessMargins,
+  'width': widthLessMargins,
+  'transform': `translate(${margin.left},${margin.top})`
+})
+
+console.log('resizedWidth')
+console.log(resizedWidth)
+
+
+var format = d3.format(",d");
+
+var color = d3.scaleSequential(d3.interpolateMagma)
+    .domain([-4, 4]);
+
+var stratify = d3.stratify().parentId(d => d.parent);
+
+var pack = d3.pack()
+    .size([widthLessMargins - 2, heightLessMargins - 2])
+    .padding(3);
+
+let makeRoot = (data) => {
+  return stratify(data)
+    .id(d => d.name)
+    .parentId(d => d.parent)
+    .sum(valFn)
+    .sort(sortFn);
+}
+
+let hovered = (hover) => {
+  return d => {
+    d3.selectAll(d.ancestors().map(nodeFn)
+    ).classed("node--hover", hover);
+  }
+}
+
+// function prepData(data){
+//   var rootData = makeRoot(data);
+//   return pack(rootData);
+// }
 
 function buildChart(data){
 
   //stratify data
+  // stratify data
   var stratRootData = d3.stratify()
     .id(d => d.name)
     .parentId(d => d.parent)
     (data);
+    
+  let rootData = pack(data)
 
-  console.log('stratRootData')
-  console.log(stratRootData)
-
-  // Add the links (given by calling tree(root), which also adds positional x/y coordinates) for the nodes
-  var link = gObj.selectAll(".link")
-    .data(buildTree(stratRootData).links())
-    .enter().append("path")
-      .attrs({
-        "class": "link",
-        "d": d3.linkHorizontal()
-          .x(d => d.y)
-          .y(d => d.x)
-      });
-
-  // Add groups for each node in the hierarchy with circles and text labels
-  var node = gObj.selectAll(".node")
-    .data(stratRootData.descendants())
+console.log('rootData')
+   console.log(rootData)
+   
+  var node = svgObj.selectAll("g")
+    .data(rootData.descendants())
     .enter().append("g")
-        .attrs({
-          "class": d => "node" + (d.children ? " node--mid" : " node--final"),
-          "transform": d => `translate(${d.y},${d.x})`
-        })
+      .attrs({
+        "transform": d => `translate(${d.x},${d.y})`,
+        "class": d => "nodeGWrapper node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root")
+      })
+      .each(function(d) {
+        // console.log('each node g THIS') 
+        // console.log(this)
+        return d.thisNode = this })
+      .on("mouseover", hovered(true))
+      .on("mouseout", hovered(false));
 
-  let nodeCircle = node.append("circle").attr("r", 2.5);
-
-  let nodeTxt = node.append("text")
+  node.append("circle")
     .attrs({
-      "dy": 3,
-      "x": d => d.children ? -8 : 8,
-      'class':'nodeText'
+      "id": d => "node-" + d.id,
+      "r": d => d.r
     })
-    .style("text-anchor", d => d.children ? "end" : "start")
-    .text(setText);
+    .style("fill", d => color(d.depth))
+
+  var childlessNodes = node.filter(d => !d.children);
+
+  childlessNodes.append("clipPath")
+      .attr("id", d => "clip-" + d.id)
+    .append("use")
+      .attr("xlink:href", d => `#node-${d.id}`);
+
+  childlessNodes.append("text")
+      .attr("clip-path", d => `url(#clip-${d.id})`)
+    .selectAll("tspan")
+    .data(d => {
+      console.log('d')
+      console.log(d)
+      
+      return d.id
+      // d.id.substring(d.id.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g)})
+    })
+    .enter().append("tspan")
+      .attrs({
+        "x": 0,
+        "y": (d, i, nodes) => 13 + (i - nodes.length / 2 - 0.5) * 10
+      })
+      .text(d => d);
+
+  node.append("title")
+      .text(d => d.id + "\n" + format(d.value));
 }
 
-let {svgObj, svgW, svgH, gObj} = makeObjsFromParent('svg');
-  
-var buildTree = d3.tree()
-    .size([svgH, svgW - 160]);
+function resize(){
+    console.log('resizing!')
+    svgObj.selectAll('*').remove();
 
-d3.json("./data.json", function(error, data) {
-    if (error) throw error;
-    
-    buildChart(data);
+    let {resizedWidth, resizedHeight, widthLessMargins, heightLessMargins} = getDimsFromParent(chartDiv);
+
+      //set svg dims
+    svgObj.attrs({
+        'height': heightLessMargins,
+        'width': widthLessMargins,
+        'transform': `translate(${margin.left},${margin.top})`
+    })
+
+    //reset three dims
+    pack.size([widthLessMargins - 2, heightLessMargins - 2])
+
+    buildChart(globalData)
+
+}
+
+let globalData;
+
+d3.json("data.json", (error, data) => {
+  if (error) throw error;
+
+  globalData = data;
+  buildChart(globalData);
+  
 });
+
+// Call the resize function whenever a resize event occurs
+d3.select(window).on('resize', resize);
