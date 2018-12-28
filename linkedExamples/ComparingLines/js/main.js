16c63c64b87882670b9ec81e3ffe08916cc11562
Eric Laursen <ericjlaursen@gmail.com>
2018-12-28 09:53:43 -0500

    functioning template

--- linkedExamples/ComparingLines/js/main.js
+++ linkedExamples/ComparingLines/js/main.js
@@ -1,10 +1,17 @@
+var filteredData;
+var lineChart1,
+    lineChart2,
+    lineChart3,
+    lineChart4,
+    lineChart5;
 var parseTime = d3.timeParse("%d/%m/%Y");
 var formatTime = d3.timeFormat("%d/%m/%Y");
 
 // Event listeners
-$("#coin-select").on("change", drawChart)
-$("#var-select").on("change", drawChart)
+$("#coin-select").on("change", updateCharts)
+$("#var-select").on("change", updateCharts)
 
+// Add jQuery UI slider
 $("#date-slider").slider({
     range: true,
     max: parseTime("31/10/2017").getTime(),
@@ -14,7 +21,7 @@ $("#date-slider").slider({
     slide: function(event, ui){
         $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
         $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
-        drawChart();
+        updateCharts();
     }
 });
 
@@ -37,6 +44,18 @@ d3.json("data/data.json").then(function(data){
         });
     }
 
-    // Run the visualization for the first time
-    drawChart();
+    lineChart1 = new LineChart("#chart-area1", "bitcoin");
+    lineChart2 = new LineChart("#chart-area2", "ethereum");
+    lineChart3 = new LineChart("#chart-area3", "bitcoin_cash");
+    lineChart4 = new LineChart("#chart-area4", "litecoin");
+    lineChart5 = new LineChart("#chart-area5", "ripple");
+
 })
+
+function updateCharts(){
+    lineChart1.wrangleData()
+    lineChart2.wrangleData()
+    lineChart3.wrangleData()
+    lineChart4.wrangleData()
+    lineChart5.wrangleData()
+}
