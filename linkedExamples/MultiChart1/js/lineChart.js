35515bede97a4ffab27e1c762dd24db1910c26ae
Eric Laursen <ericjlaursen@gmail.com>
2019-01-16 13:31:16 -0500

    renders line chart functionally

--- linkedExamples/MultiChart1/js/lineChart.js
+++ linkedExamples/MultiChart1/js/lineChart.js
@@ -16,12 +16,11 @@ function formatAbbreviation(x) {
 }
 
 function filterCoinStats(data, timeVals){
-    let coinName = Object.keys(data)[0]
-    let sliderFilteredData = data[coinName].filter(function(d) {
+    let sliderFilteredData = data.filter(function(d) {
         return ((d.date >= timeVals[0]) && (d.date <= timeVals[1]))
     })
 
-    return { coinName, sliderFilteredData }
+    return { sliderFilteredData }
 }
 
 function setFocusDisplay(setting, coinName, arr){
@@ -43,167 +42,153 @@ let xAxisObj = d3.axisBottom().ticks(4);
 let xAxisG, yAxisG, gObj, svgObj, linePath, focus, margin = { left:50, right:20, top:50, bottom:20 };
 const t = function() { return d3.transition().duration(450); }
 const bisectDate = d3.bisector(function(d) { return d.date; }).left;
-const lineHeight = 250,
-    lineWidth = 300,
-    heightLM = lineHeight - margin.top - margin.bottom,
-    widthLM = lineWidth - margin.left - margin.right;
 
-LineChart = function(_parentElement){
-    this.parentElement = _parentElement;
+// Filter data based on selections
+const height = 550, 
+      width = 800, 
+      heightLM = height - margin.top - margin.bottom,
+      widthLM = width - margin.left - margin.right;
 
-    this.initVis();
-};
+const initLine = function(parent, coinData){
 
-LineChart.prototype.initVis = function(parent){
-    var vis = this;
-
-    svgObj = d3.select(vis.parentElement)
+    let coinName = state.activeCoin
+    svgObj = d3.select(parent)
         .append("svg")
         .attrs({
-            "width": lineWidth + margin.left + margin.right,
-            "height": lineHeight + margin.top + margin.bottom
+            "width": width,
+            "height": height,
+            'class': `${coinName}SvgWrapper svgWrapper`
         });
-    vis.g = svgObj.append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    vis.t = function() { return d3.transition().duration(1000); }
+    thisGObj = appendAndTransG(svgObj, `translate(${margin.left},${margin.top})`, `${coinName}GWrapper`);
 
-    vis.bisectDate = d3.bisector(function(d) { return d.date; }).left;
+    // thisGObj.append("text")
+    //     .attrs({
+    //         "x": widthLM/2,
+    //         "y": 0,
+    //         "text-anchor": "middle"
+    //     })
+    //     .text(Object.keys(coinData))
 
-    vis.linePath = vis.g.append("path")
-        .attrs({
-            "class": "line",
-            "fill": "none",
-            "stroke-width": "3px"
-        })
+    state.xScales[coinName].range([0, widthLM]);
+    state.yScales[coinName].range([heightLM, 0]);
 
-    vis.yLabel = vis.g.append("text")
-        .attrs({
-            "class": "y axisLabel",
-            "transform": "rotate(-90)",
-            "y": -60,
-            "x": -170,
-            "font-size": "20px",
-            "text-anchor": "middle"
-        })
-        .text("Price (USD)")
-
-    vis.x = d3.scaleTime().range([0, lineWidth]);
-    vis.y = d3.scaleLinear().range([lineHeight, 0]);
-
-    vis.xAxis = vis.g.append("g")
-        .attrs({
-            "class": "x axis",
-            "transform": `translate(0,${lineHeight})`
-        });
-    vis.yAxis = vis.g.append("g")
-        .attr("class", "y axis");
-
-    
-    vis.coin = $("#coin-select").val()
-    vis.yVariable = $("#var-select").val()
-
-    // Filter data based on selections
-    vis.sliderValues = $("#date-slider").slider("values")
-    vis.dataFiltered = state.filteredData[vis.coin].filter(function(d) {
-        return ((d.date >= vis.sliderValues[0]) && (d.date <= vis.sliderValues[1]))
-    })
-
-    vis.updateVis();
-
+    xAxisG = appendAndTransG(thisGObj, `translate(0,${heightLM})`, `${coinName}xAxisG x axis`);
+    yAxisG = appendAndTransG(thisGObj, null, `${coinName}yAxisG y axis`);
+        
+    updateVis(coinData);
 };
 
+const updateVis = function(coinData, sliderTimeVals){
+    let coinName = state.activeCoin;
 
-
-LineChart.prototype.updateVis = function(){
-    var vis = this;
-
-    // Update scales
-    vis.x.domain(d3.extent(vis.dataFiltered, function(d) { return d.date; }));
-    vis.y.domain([d3.min(vis.dataFiltered, function(d) { return d[vis.yVariable]; }) / 1.005, 
-        d3.max(vis.dataFiltered, function(d) { return d[vis.yVariable]; }) * 1.005]);
-
-
-    function formatAbbreviation(x) {
-      var s = formatSi(x);
-      switch (s[s.length - 1]) {
-        case "G": return s.slice(0, -1) + "B";
-        case "k": return s.slice(0, -1) + "K";
-      }
-      return s;
+    /*
+        set chart params if not already selected
+    */
+    if(coinName == null){
+        coinName = $("#measurement-select").val()
     }
+    
+    let timeVals = (!sliderTimeVals) ? [1368331200000, 1509422400000] : sliderTimeVals;
+    
+    //get filtered chart data from parameters
+    let { sliderFilteredData } = filterCoinStats(coinData, timeVals)
+
+    /*
+        Update scales
+    */
+    
+    
+    state.xScales[coinName].domain(d3.extent(sliderFilteredData, d => d.date));
+    state.yScales[coinName].domain([d3.min(sliderFilteredData, d => d[state.yVariable]) / 1.005, 
+        d3.max(sliderFilteredData, d => d[state.yVariable]) * 1.005]);
 
     // Update axes
-    xAxisObj.scale(vis.x);
-    vis.xAxis.transition(vis.t()).call(xAxisObj);
-    yAxisObj.scale(vis.y);
-    vis.yAxis.transition(vis.t()).call(yAxisObj.tickFormat(formatAbbreviation));
+    let thisXAxisG = d3.select(`.${coinName}xAxisG`),
+        thisYAxisG = d3.select(`.${coinName}yAxisG`);
+    xAxisObj.scale(state.xScales[coinName]);
+    yAxisObj.scale(state.yScales[coinName]);
+    thisXAxisG.transition(t()).call(xAxisObj);
+    thisYAxisG.transition(t()).call(yAxisObj.tickFormat(formatAbbreviation));
+    
+    //remove previous path, focuses, & overlays
+    let thisGObj = d3.select(`g.${coinName}GWrapper`)
+    thisGObj.selectAll(`path`).remove()
+    thisGObj.selectAll(`.focus${coinName}`).remove()
+    d3.select(`.${coinName}ovly`).remove();
 
-    // Discard old tooltip elements
-    d3.select(".focus").remove();
-    d3.select(".overlay").remove();
+    const line = d3.line()
+        .x(function(d) { return state.xScales[coinName](d.date); })
+        .y(function(d) { return state.yScales[coinName](d[state.yVariable]); 
+    });
 
-    var focus = vis.g.append("g")
-        .attr("class", "focus")
-        .style("display", "none");
+    linePath = thisGObj.append("path")
+        .attr("class", `line ${coinName} line${coinName} chartLine`);
+
+    linePath.transition(t())
+        .attr("d", line(sliderFilteredData));
+
+    /*
+        build & configthe 'focus' elements, for mouseover interaction
+        x-hover line
+        y-hover line
+        hover circle
+        hover text
+    */
+    focus = appendAndTransG(thisGObj, null, `focus${coinName}`).style("display", "none");
 
     focus.append("line")
         .attrs({
-            "class": "x-hover-line hover-line",
+            "class": `${coinName}xHovLine x-hover-line hover-line`,
             "y1": 0,
-            "y2": lineHeight
+            "y2": heightLM
         });
 
     focus.append("line")
-        .attr({
-            "class": "y-hover-line hover-line",
+        .attrs({
+            "class": `${coinName}yHovLine y-hover-line hover-line`,
             "x1": 0,
-            "x2": lineWidth
+            "x2": widthLM
         });
 
     focus.append("circle")
-        .attr("r", 5);
+        .attr("r", 8);
 
     focus.append("text")
-        .attr("x", 15)
-        .attr("dy", ".31em");
+        .attrs({
+            "x": 15,
+            "dy": ".31em",
+        });
 
-    svgObj.append("rect")
+    /*
+        build & config the hidden rect for hover interaction
+    */
+
+    d3.select(`.${coinName}SvgWrapper`).append("rect")
         .attrs({
             "transform": `translate(${margin.left},${margin.top})`,
-            "class": "overlay",
-            "width": lineWidth,
-            "height": lineHeight
+            "class": `${coinName}ovly overlay`,
+            "width": widthLM,
+            "height": heightLM
         })
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mouseover", () => setFocusDisplay(null, coinName, [.2,1]))
+        .on("mouseout", () => setFocusDisplay('none', coinName, [1,1]))
         .on("mousemove", mousemove);
 
     function mousemove() {
-        var x0 = vis.x.invert(d3.mouse(this)[0]),
-            i = vis.bisectDate(vis.dataFiltered, x0, 1),
-            d0 = vis.dataFiltered[i - 1],
-            d1 = vis.dataFiltered[i],
+        let thisFocus = d3.select(`.focus${coinName}`)
+        
+        var x0 = state.xScales[coinName].invert(d3.mouse(this)[0]),
+            i = bisectDate(sliderFilteredData, x0, 1),
+            d0 = sliderFilteredData[i - 1],
+            d1 = sliderFilteredData[i],
             d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0;
-        focus.attr("transform", "translate(" + vis.x(d.date) + "," + vis.y(d[vis.yVariable]) + ")");
-        focus.select("text").text(function() { return formatDollar(d[vis.yVariable].toFixed(2)); });
-        focus.select(".x-hover-line").attr("y2", lineHeight - vis.y(d[vis.yVariable]));
-        focus.select(".y-hover-line").attr("x2", -vis.x(d.date));
+            
+        thisFocus.attr("transform", `translate(${state.xScales[coinName](d.date)},${state.yScales[coinName](d[state.yVariable])})`);
+        thisFocus.select("text").text(() => {
+            return formatDollar(d[state.yVariable].toFixed(2))
+        });
+        thisFocus.select(`.${coinName}xHovLine`).attr("y2", heightLM - state.yScales[coinName](d[state.yVariable]));
+        thisFocus.select(`.${coinName}yHovLine`).attr("x2", -state.xScales[coinName](d.date));
     }
-
-    // Update y-axis label
-    var newLabel = (vis.yVariable == "price_usd") ? "Price (USD)" :
-        ((vis.yVariable == "market_cap") ? "Market Capitalization (USD)" :
-            "24 Hour Trading Volume (USD)")
-    vis.yLabel.text(newLabel)
-
-    var line = d3.line()
-        .x(function(d) { return vis.x(d.date); })
-        .y(function(d) { return vis.y(d[vis.yVariable]); });
-
-    vis.g.select(".line")
-        .attr("stroke", color(vis.coin))
-        .transition(vis.t)
-        .attr("d", line(vis.dataFiltered));
-
 };
