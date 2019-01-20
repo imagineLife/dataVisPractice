7d77afb2fa1dd4875137189e768a0e8109938484
Eric Laursen <ericjlaursen@gmail.com>
2019-01-20 17:06:32 -0500

    sets up initTimeline

--- linkedExamples/MultiChartWithBrushing/js/timeline.js
+++ linkedExamples/MultiChartWithBrushing/js/timeline.js
@@ -1,5 +1,39 @@
 function initTimeline(parentDiv){
-	console.log('parentDiv')
-	console.log(parentDiv)
-	
+	let wLM = state.timeline.w - state.timeline.margin.r - state.timeline.margin.l;
+	let hLM = state.timeline.h - state.timeline.margin.t - state.timeline.margin.b;
+
+	//set timelineSVG vals
+	state.timeline.svg = d3.select(parentDiv).append('svg').attrs({
+		'height': state.timeline.h,
+		'width': state.timeline.w,
+		'class': 'timelineSVG'
+	})
+
+	//set timeline G vals
+	state.timeline.gObj = state.timeline.svg.append('g')
+		.attr('class', 'timelineG')
+
+	//set timeline scales
+	state.timeline.xScale.range([0, state.timeline.w])
+	state.timeline.yScale.range([state.timeline.h, 0])
+
+	//build axis elements
+	state.xAxisG = state.timeline.gObj.append('g')
+		.attrs({
+			'class' : 'timelineXAxisG',
+			'transform' : `translate(0, ${state.timeline.h})`
+		})
+
+	state.arePath = state.timeline.gObj
+		.append('path')
+		.attrs({
+			'fill': '#ccc',
+			'class': 'timelinePath'
+		})
+
+	updateTimeLine();
+}
+
+function updateTimeLine(){
+
 }
