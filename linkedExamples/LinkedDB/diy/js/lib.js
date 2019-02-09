0f501026058ddeb86a69c339c0b28a7286d55b8a
Eric Laursen <ericjlaursen@gmail.com>
2019-02-09 13:21:44 -0500

    adds some fns to lib

--- linkedExamples/LinkedDB/diy/js/lib.js
+++ linkedExamples/LinkedDB/diy/js/lib.js
@@ -1,4 +1,6 @@
-const appendToParent = (parent, cl, trans) => {
+let lib = {};
+
+lib.appendToParent = (parent, cl, trans) => {
 	return parent.append("g")
         .attrs({
             "class": cl,
@@ -6,3 +8,40 @@ const appendToParent = (parent, cl, trans) => {
         });
 }
 	
+lib.makeLegendArr = (colorScale, strArray) => {
+    return strArray.map(str => {
+        let capdWord = str.charAt(0).toUpperCase() + str.slice(1)
+        return {label: capdWord, color: colorScale(str)}
+    })
+}
+
+lib.addLegendToParent = (colorScale, legendParentG, customClassName, legendArr) => {
+
+    var areaLegend = lib.appendToParent(legendParentG, customClassName, `translate(${50},${-25})`)
+
+    var legendGWrapper = areaLegend.selectAll(".customClassName")
+        .data(legendArr)
+        .enter().append("g")
+            .attrs({
+                "class": "legendGWrapper",
+                "transform": (d, i) => `translate(${(i * 150)},${(0)})`
+            });
+        
+    let legendRects = legendGWrapper.append("rect")
+        .attrs({
+            "class": "legendRect",
+            "width": 10,
+            "height": 10,
+            "fill": d => d.color,
+            "fill-opacity": 0.5
+        });
+
+    let legendTexts = legendGWrapper.append("text")
+        .attrs({
+            "class": "legendText",
+            "x": 20,
+            "y": 10,
+            "text-anchor": "start"
+        })
+        .text(d => d.label); 
+}
