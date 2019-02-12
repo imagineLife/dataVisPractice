f354e69f4bc1fb8c04e7e0412b471512db161222
Eric Laursen <ericjlaursen@gmail.com>
2019-02-12 06:13:29 -0500

    uses stated margins

--- linkedExamples/LinkedDB/diy/js/timeline.js
+++ linkedExamples/LinkedDB/diy/js/timeline.js
@@ -5,18 +5,14 @@ Timeline = function(_parentElement){
 Timeline.prototype.initTimeline = function(parent){
     var vis = this;
 
-    vis.margin = {top: 0, right: 100, bottom: 20, left: 80};
-    vis.width = 800 - vis.margin.left - vis.margin.right;
-    vis.height = 100 - vis.margin.top - vis.margin.bottom;
+    vis.width = 800 - state.tlObj.m.left - state.tlObj.m.right;
+    vis.height = 100 - state.tlObj.m.top - state.tlObj.m.bottom;
 
     vis.svg = d3.select(parent).append("svg")
-        .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .attr("width", vis.width + state.tlObj.m.left + state.tlObj.m.right)
+        .attr("height", vis.height + state.tlObj.m.top + state.tlObj.m.bottom)
 
     vis.t = () => { return d3.transition().duration(1000); }
-
-    vis.g = vis.svg.append("g")
-            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
     vis.x = d3.scaleTime()
         .range([0, vis.width]);
@@ -27,9 +23,8 @@ Timeline.prototype.initTimeline = function(parent){
     vis.xAxisCall = d3.axisBottom()
         .ticks(4);
 
-    vis.xAxis = vis.g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + vis.height +")");
+    vis.g = lib.appendToParent(vis.svg, null, `translate(${state.tlObj.m.left},${state.tlObj.m.top})`);
+    vis.xAxis = lib.appendToParent(vis.g, 'x axis', `translate(0,${vis.height})`);
 
     vis.areaPath = vis.g.append("path")
         .attr("fill", "#ccc");
@@ -41,9 +36,7 @@ Timeline.prototype.initTimeline = function(parent){
         .on("brush end", brushed)
 
     // Append brush component
-    vis.brushComponent = vis.g.append("g")
-        .attr("class", "brush")
-        .call(vis.brush);
+    vis.brushComponent = lib.appendToParent(vis.g, 'brush', null).call(vis.brush);
 
     vis.wrangleData();
 };
@@ -51,31 +44,29 @@ Timeline.prototype.initTimeline = function(parent){
 Timeline.prototype.wrangleData = function(){
     var vis = this;
 
-    vis.variable = "call_revenue"
-
-    vis.dayNest = d3.nest()
+    let dayNest = d3.nest()
         .key(function(d){ return formatTime(d.date); })
         .entries(state.srcData)
 
-    vis.dataFiltered = vis.dayNest
+    let dataFiltered = dayNest
         .map(function(day){
             return {
                 date: day.key,
                 sum: day.values.reduce(function(accumulator, current){
-                    return accumulator + current[vis.variable]
+                    return accumulator + current["call_revenue"]
                 }, 0)               
             }
 
         })
 
-    vis.updateVis();
+    vis.updateVis(dataFiltered);
 }
 
-Timeline.prototype.updateVis = function(){
+Timeline.prototype.updateVis = function(srcData){
     var vis = this;
 
-    vis.x.domain(d3.extent(vis.dataFiltered, (d) => { return parseTime(d.date); }));
-    vis.y.domain([0, d3.max(vis.dataFiltered, (d) => d.sum) ])
+    vis.x.domain(d3.extent(srcData, (d) => { return parseTime(d.date); }));
+    vis.y.domain([0, d3.max(srcData, (d) => d.sum) ])
 
     vis.xAxisCall.scale(vis.x)
 
@@ -92,6 +83,6 @@ Timeline.prototype.updateVis = function(){
         .y1((d) => { return vis.y(d.sum); })
 
     vis.areaPath
-        .data([vis.dataFiltered])
+        .data([srcData])
         .attr("d", vis.area);
 }
