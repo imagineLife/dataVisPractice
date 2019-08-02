fdaac2f9cc0be2ba71431b62cea5e518995a292f
Eric Laursen <ericjlaursen@gmail.com>
2019-08-02 12:00:07 -0400

    adds width-transition && disable/enable buttons

--- animation/tween/index.js
+++ animation/tween/index.js
@@ -1,7 +1,12 @@
 const div = d3.select('.chartDiv')
 let transDur = 4000;
-const btn = d3.select('#trans-btn')
-let curColor = 'steelblue'
+const colorBtn = d3.select('#re-color')
+const widthBtn = d3.select('#re-width')
+let state = {
+	color: 'steelblue',
+	width: 200
+}
+
 //svg
 let svg = div.append('svg').attrs({
 	width:800,
@@ -12,16 +17,34 @@ let svg = div.append('svg').attrs({
 let rect = svg.append('rect').attrs({
 	height: 50,
 	width: 200,
-	fill: curColor,
+	fill: state.color,
 	class: 'this-rect'
 })
 
-btn.on('click', () => {
-	curColor = curColor === 'steelblue' ? 'yellow' : 'steelblue';
+colorBtn.on('click', () => {
+	d3.selectAll('button').attr('disabled', true)
+	state.color = state.color === 'steelblue' ? 'yellow' : 'steelblue';
 	console.log('clicked!');
-	return d3.selectAll(".this-rect")
+	d3.select(".this-rect")
 	.transition()
     .duration(transDur)
     .ease(d3.easeLinear)
-    .style("fill", curColor);
+    .style("fill", state.color);
+    setTimeout(() => {
+    	d3.selectAll('button').attr('disabled', null)
+    }, transDur)
+})
+
+widthBtn.on('click', () => {
+	d3.selectAll('button').attr('disabled', true)
+	state.width = state.width === 200 ? 450 : 200;
+	console.log('clicked!');
+	d3.select(".this-rect")
+	.transition()
+    .duration(transDur)
+    .ease(d3.easeLinear)
+    .attr("width", state.width);
+    setTimeout(() => {
+    	d3.selectAll('button').attr('disabled', null)
+    }, transDur)
 })
