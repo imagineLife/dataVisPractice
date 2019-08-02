4b62812e9ebb7bf6235b75667c62e3d115eaf266
Eric Laursen <ericjlaursen@gmail.com>
2019-08-02 13:07:21 -0400

    includes simplified no-comment fn

--- animation/tween/index.js
+++ animation/tween/index.js
@@ -31,15 +31,16 @@ var background = g.selectAll("path")
     .attr("d", arc);
 
 // Add the foreground arc in orange, currently showing 12.7%.
-var foreground = g.append("path")
+var foregroundArc = g.append("path")
     .datum({endAngle: 0.127 * magicNumber})
     .style("fill", "orange")
     .attr("d", arc);
 
 // Every so often, start a transition to a new random angle. The attrTween
 // definition is encapsulated in a separate function (a closure) below.
-d3.interval(function() {
-  foreground.transition()
+d3.interval(() => {
+  let newLength = Math.random() * magicNumber
+  foregroundArc.transition()
       .duration(750)
 
         /* 
@@ -57,49 +58,74 @@ d3.interval(function() {
           - the ending angle (the 'newAngle' argument to the enclosing function)
             - Math.random() * magicNumber
         */
-      .attrTween("d", arcTween(Math.random() * magicNumber));
+      .attrTween("d", arcTween(newLength));
 }, 2000);
 
 // Returns a tween for a transition’s "d" attribute, transitioning any selected
 // arcs from their current angle to the specified new angle.
 function arcTween(newAngle) {
 
-  return function(d) {
-
-    // To interpolate between the two angles, we use the default d3.interpolate.
-    // (Internally, this maps to d3.interpolateNumber, since both of the
-    // arguments to d3.interpolate are numbers.) The returned function takes a
-    // single argument t and returns a number between the starting angle and the
-    // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
-    // newAngle; and for 0 < t < 1 it returns an angle in-between.
+  return (d) => {
     var interpolate = d3.interpolate(d.endAngle, newAngle);
-
-    // The return value of the attrTween is also a function: the function that
-    // we want to run for each tick of the transition. Because we used
-    // attrTween("d"), the return value of this last function will be set to the
-    // "d" attribute at every tick. (It’s also possible to use transition.tween
-    // to run arbitrary code for every tick, say if you want to set multiple
-    // attributes from a single function.) The argument t ranges from 0, at the
-    // start of the transition, to 1, at the end.
-    return function(t) {
-
-      // Calculate the current arc angle based on the transition time, t. Since
-      // the t for the transition and the t for the interpolate both range from
-      // 0 to 1, we can pass t directly to the interpolator.
-      //
-      // Note that the interpolated angle is written into the element’s bound
-      // data object! This is important: it means that if the transition were
-      // interrupted, the data bound to the element would still be consistent
-      // with its appearance. Whenever we start a new arc transition, the
-      // correct starting angle can be inferred from the data.
+    return (t) => {
       d.endAngle = interpolate(t);
+      return arc(d);
+    };
+  };
+}
+
+//function arcTween(newAngle) {
+
+//  return function(d) {
+
+    /* 
+    returns between 0 - 1
+
+     To interpolate between the two angles, we use the default d3.interpolate.
+     (Internally, this maps to d3.interpolateNumber, since both of the
+     arguments to d3.interpolate are numbers.) 
+     The returned function 
+     - takes a single argument t 
+     - returns a number between the starting angle and the ending angle 
+     - WHEN t = 0 
+      - it returns d.endAngle; 
+     - WHEN t = 1, it returns newAngle; 
+     - for 0 < t < 1 it returns an angle in-between.
+    */
+
+//    var interpolate = d3.interpolate(d.endAngle, newAngle);
+
+    /* 
+      The return value of the attrTween is also a function: 
+      - the function that we want to run for each tick of the transition 
+      - Because we used attrTween("d"), 
+        the return value of this last function will be 
+        - set to the "d" attribute at every tick 
+        - It’s also possible to use transition.tween to run arbitrary code for every tick
+            - say if you want to set multiple attributes from a single function 
+        - The argument t ranges from 0, at the start of the transition, to 1, at the end.
+    */
+//    return function(t) {
+
+      /*
+       Calculate the current arc angle based on the transition time, t. Since
+       the t for the transition and the t for the interpolate both range from
+       0 to 1, we can pass t directly to the interpolator.
+      
+       Note that the interpolated angle is written into the element’s bound
+       data object! This is important: it means that if the transition were
+       interrupted, the data bound to the element would still be consistent
+       with its appearance. Whenever we start a new arc transition, the
+       correct starting angle can be inferred from the data.
+      */
+//      d.endAngle = interpolate(t);
 
       // Lastly, compute the arc path given the updated data! In effect, this
       // transition uses data-space interpolation: the data is interpolated
       // (that is, the end angle) rather than the path string itself.
       // Interpolating the angles in polar coordinates, rather than the raw path
       // string, produces valid intermediate arcs during the transition.
-      return arc(d);
-    };
-  };
-}
+//      // return arc(d);
+//    };
+//  };
+//}
