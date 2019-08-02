2bc6240c879bc6a5b4c86060f3449035832d73dd
Eric Laursen <ericjlaursen@gmail.com>
2019-08-02 14:44:06 -0400

    introduces dataJoin && enter rendering logic

--- slope/index.js
+++ slope/index.js
@@ -1,8 +1,137 @@
 const state = {
 	width: 600,
 	height: 500,
-	margin: {top: 20, right: 100, bottom: 30, left: 150}
+	margin: {top: 20, right: 100, bottom: 30, left: 150},
+	yScale: null
 };
+
+function enterFn(e){
+
+	const { margin, width, height } = state;
+	// Calculate area chart takes up out of entire svg
+	var chartHeight = height - margin.top - margin.bottom;
+	var chartWidth = width - margin.left - margin.right;
+
+	// Create bottom area denoting years
+	e.append("line")
+		.attr('x1', margin.left)
+		.attr('x2', margin.left)
+		.attr('y1', height - margin.bottom)
+		.attr('y2', height - margin.bottom * 0.7)
+		.attr('stroke', 'grey')
+		.attr('stroke-width', '2px');
+
+	e.append("line")
+		.attr('x1', chartWidth + margin.right * 0.75)
+		.attr('x2', chartWidth + margin.right * 0.75)
+		.attr('y1', height - margin.bottom)
+		.attr('y2', height - margin.bottom * 0.7)
+		.attr('stroke', 'grey')
+		.attr('stroke-width', '2px');
+
+	e.append("line")
+		.attr('x1', margin.left)
+		.attr('x2', chartWidth + margin.right * 0.75)
+		.attr('y1', height - margin.bottom * 0.7)
+		.attr('y2', height - margin.bottom * 0.7)
+		.attr('stroke', 'grey')
+		.attr('stroke-width', '2px');
+
+	e.append('text')
+		.text('2016')
+		.attr('class', 'neutral')
+		.attr('x', margin.left)
+		.attr('y', height - margin.bottom * 0.05)
+		.attr('text-anchor', 'start');
+
+	e.append('text')
+		.text('2017')
+		.attr('class', 'neutral')
+		.attr('x', chartWidth + margin.right * 0.75)
+		.attr('y', height - margin.bottom * 0.05)
+		.attr('text-anchor', 'end');
+
+	e.selectAll('text-comments')
+		.append('text')
+		.text(function(d) {
+			return d.Comments;
+		})
+		.attr('class', 'neutral')
+		.attr('text-anchor', 'start')
+		.attr('x', chartWidth + margin.right)
+		.attr('y', function(d) {
+			return margin.top + chartHeight - state.yScale(d.After);
+		})
+		.attr('dy', '.25em')
+		.call(wrap, margin.right);
+
+// Create slopegraph lines
+	e.append('line')
+		.attr('class', function(d) {
+			return 'slope-line ' + d.delta;
+		})
+		.attr('x1', margin.left)
+		.attr('x2', chartWidth + margin.right * 0.75)
+		.attr('y1', function(d) { 
+			return margin.top + chartHeight - state.yScale(d.Before);
+		})
+		.attr('y2', function(d) { 
+			return margin.top + chartHeight - state.yScale(d.After);
+		});
+	
+	// Create slopegraph left circles
+	e.append('circle')
+		.attr('class', function(d) {
+			return d.delta;
+		})
+		.attr('cx', margin.left)
+		.attr('cy', function(d) { 
+			return margin.top + chartHeight - state.yScale(d.Before);
+		})
+		.attr('r', 6);
+
+	// Create slopegraph right circles
+	e.append('circle')
+		.attr('class', function(d) {
+			return d.delta;
+		})
+		.attr('cx',chartWidth + margin.right * 0.75)
+		.attr('cy', function(d) { 
+			return margin.top + chartHeight - state.yScale(d.After);
+		})
+		.attr('r', 6);
+
+		// Create slopegraph labels
+		e.append('text')
+		.text(d =>d.Tool)
+		.attrs({
+			'class': d => `label ${d.delta}`,
+			'text-anchor': 'end',
+			'x': margin.left * .6,
+			'y': d => margin.top + chartHeight - state.yScale(d.Before),
+			'dy': '.35em'
+		})
+	
+	// Create slopegraph left value labels
+		e.append('text')
+		.attrs({
+			'class': d => d.delta,
+			'text-anchor': 'end',
+			'x': margin.left * .85,
+			'y': d => margin.top + chartHeight - state.yScale(d.Before), 
+			'dy': '.35em'
+		})
+		.text(d => Math.round(d.Before) + '%');
+
+	// Create slopegraph left value labels
+		e.append('text')
+		.attr('class', d => d.delta)
+		.attr('text-anchor', 'start')
+		.attr('x', chartWidth + margin.right)
+		.attr('y', d => margin.top + chartHeight - state.yScale(d.After))
+		.attr('dy', '.35em')
+		.text(d => Math.round(d.After) + '%');
+}
 
 function buildChart(srcData, opts){
 
@@ -12,6 +141,10 @@ function buildChart(srcData, opts){
 	var chartHeight = height - margin.top - margin.bottom;
 	var chartWidth = width - margin.left - margin.right;
 
+	// Create scale for positioning data correctly in chart
+	state.yScale = d3.scaleLinear()
+		.domain([6, 53])
+		.range([margin.bottom, chartHeight]);
 
 	var svg = d3.select('#chart')
 		.append('svg')
@@ -20,163 +153,12 @@ function buildChart(srcData, opts){
 			'height': height
 		});
 
-	// Create scale for positioning data correctly in chart
-	var yScale = d3.scaleLinear()
-		.domain([6, 53])
-		.range([margin.bottom, chartHeight]);
+	let dataJoin = svg.selectAll('g')
+	.data(srcData)
+	dataJoin.join(enterFn)
+}
 
-	// Create slopegraph labels
-	svg.selectAll('text.labels')
-		.data(srcData)
-		.enter()
-		.append('text')
-		.text(function(d) {
-			return d.Tool
-		})
-		.attrs({
-			'class': d => `label ${d.delta}`,
-			'text-anchor': 'end',
-			'x': margin.left * .6,
-			'y': d => margin.top + chartHeight - yScale(d.Before),
-			'dy': '.35em'
-		})
-	
-	// Create slopegraph left value labels
-	svg.selectAll('text.leftvalues')
-		.data(srcData)
-		.enter()
-		.append('text')
-		.attrs({
-			'class': d => d.delta,
-			'text-anchor': 'end',
-			'x': margin.left * .85,
-			'y': d => margin.top + chartHeight - yScale(d.Before), 
-			'dy': '.35em'
-		})
-		
-		.text(function(d) {
-			return Math.round(d.Before) + '%'
-		});
-
-	// Create slopegraph left value labels
-	svg.selectAll('text.rightvalues')
-		.data(srcData)
-		.enter()
-		.append('text')
-		.attr('class', function(d) {
-			return d.delta;
-		})
-		.attr('text-anchor', 'start')
-		.attr('x', chartWidth + margin.right)
-		.attr('y', function(d) { 
-			return margin.top + chartHeight - yScale(d.After);
-		})
-		.attr('dy', '.35em')
-		.text(function(d) {
-			return Math.round(d.After) + '%'
-		});
-
-	// Create slopegraph lines
-	svg.selectAll('line.slope-line')
-		.data(srcData)
-		.enter()
-		.append('line')
-		.attr('class', function(d) {
-			return 'slope-line ' + d.delta;
-		})
-		.attr('x1', margin.left)
-		.attr('x2', chartWidth + margin.right * 0.75)
-		.attr('y1', function(d) { 
-			return margin.top + chartHeight - yScale(d.Before);
-		})
-		.attr('y2', function(d) { 
-			return margin.top + chartHeight - yScale(d.After);
-		});
-	
-	// Create slopegraph left circles
-	svg.selectAll('line.left-circle')
-		.data(srcData)
-		.enter()
-		.append('circle')
-		.attr('class', function(d) {
-			return d.delta;
-		})
-		.attr('cx', margin.left)
-		.attr('cy', function(d) { 
-			return margin.top + chartHeight - yScale(d.Before);
-		})
-		.attr('r', 6);
-
-	// Create slopegraph right circles
-	svg.selectAll('line.left-circle')
-		.data(srcData)
-		.enter()
-		.append('circle')
-		.attr('class', function(d) {
-			return d.delta;
-		})
-		.attr('cx',chartWidth + margin.right * 0.75)
-		.attr('cy', function(d) { 
-			return margin.top + chartHeight - yScale(d.After);
-		})
-		.attr('r', 6);
-
-	// Create bottom area denoting years
-	svg.append("line")
-		.attr('x1', margin.left)
-		.attr('x2', margin.left)
-		.attr('y1', height - margin.bottom)
-		.attr('y2', height - margin.bottom * 0.7)
-		.attr('stroke', 'grey')
-		.attr('stroke-width', '2px');
-
-	svg.append("line")
-		.attr('x1', chartWidth + margin.right * 0.75)
-		.attr('x2', chartWidth + margin.right * 0.75)
-		.attr('y1', height - margin.bottom)
-		.attr('y2', height - margin.bottom * 0.7)
-		.attr('stroke', 'grey')
-		.attr('stroke-width', '2px');
-
-	svg.append("line")
-		.attr('x1', margin.left)
-		.attr('x2', chartWidth + margin.right * 0.75)
-		.attr('y1', height - margin.bottom * 0.7)
-		.attr('y2', height - margin.bottom * 0.7)
-		.attr('stroke', 'grey')
-		.attr('stroke-width', '2px');
-
-	svg.append('text')
-		.text('2016')
-		.attr('class', 'neutral')
-		.attr('x', margin.left)
-		.attr('y', height - margin.bottom * 0.05)
-		.attr('text-anchor', 'start');
-
-	svg.append('text')
-		.text('2017')
-		.attr('class', 'neutral')
-		.attr('x', chartWidth + margin.right * 0.75)
-		.attr('y', height - margin.bottom * 0.05)
-		.attr('text-anchor', 'end');
-
-	svg.selectAll('text-comments')
-		.data(srcData)
-		.enter()
-		.append('text')
-		.text(function(d) {
-			return d.Comments;
-		})
-		.attr('class', 'neutral')
-		.attr('text-anchor', 'start')
-		.attr('x', chartWidth + margin.right)
-		.attr('y', function(d) {
-			return margin.top + chartHeight - yScale(d.After);
-		})
-		.attr('dy', '.25em')
-		.call(wrap, margin.right);
-
-	function wrap(text, width) {
+function wrap(text, width) {
 	  text.each(function() {
 	    var text = d3.select(this),
 	        words = text.text().split(/\s+/).reverse(),
@@ -199,7 +181,6 @@ function buildChart(srcData, opts){
 	    }
 	  });
 	}
-}
 
 function prepData(srcData){
 		// assign a DELTA key to each element
@@ -216,9 +197,6 @@ function prepData(srcData){
 	    }
 	}
 
-	/*
-			DATA PREP
-	*/
 	for (var i = 1; i < (srcData.length - 1); i++) {
 		if ((srcData[i]['BeforeY']-srcData[i+1]['BeforeY']) < 15) {
 			if ((srcData[i-1]['BeforeY']-srcData[i]['BeforeY']) < 15) {
@@ -229,16 +207,10 @@ function prepData(srcData){
 		}
 	}
 
-	/*
-			DATA PREP
-	*/
 	srcData.sort(function(a, b) {
 		return b.After-a.After;
 	})
 
-	/*
-			DATA PREP
-	*/
 	for (var i = 1; i < (srcData.length - 1); i++) {
 
 		if ((srcData[i]['AfterY']-srcData[i+1]['AfterY']) < 15) {
@@ -252,9 +224,6 @@ function prepData(srcData){
 		} 
 	}
 
-	/*
-			DATA PREP
-	*/
 	srcData.sort(function(a, b) {
 		return b.Before-a.Before;
 	})
