a72b8f23ad2e99286822428b4ad6daad07477ba7
Eric Laursen <ericjlaursen@gmail.com>
2019-08-05 09:35:36 -0400

    svg && dimensions

--- boxplot/index.js
+++ boxplot/index.js
@@ -5,6 +5,18 @@ const state = {
 		b: 30,
 		l: 40
 	},
-	w: 400,
-	h: 400
+	w: 450,
+	h: 450
 }
+
+//dimensions-les--margins
+// https://bl.ocks.org/mbostock/3019563
+const wLM = state.w - state.m.l - state.m.r;
+const hLM = state.w - state.m.t - state.m.b;
+
+const svg = d3.select('#chartDiv').append('svg')
+	.attrs({
+		class: 'svg-wrapper',
+		width: wLM,
+		height: hLM
+	})
