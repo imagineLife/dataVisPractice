01923f02f8541266e1fa5d9d998baf7243569ec6
Eric Laursen <ericjlaursen@gmail.com>
2020-06-20 08:31:58 -0400

    code cleanup

--- mapping/US/plotLatLon/index.js
+++ mapping/US/plotLatLon/index.js
@@ -1,11 +1,29 @@
 const buildChart = async () => {
-        
+
+  const enterPlaces = e => {
+    e.append("circle", ".pin")
+    .attr("r", radius)
+    .attr("transform" d => {
+      const { location: { latitude, longitude}} = d
+      return `translate(${ projection([ longitude,latitude ]) })`
+    }) 
+  }        
   // "state" data
+
+  const enterGroups = e => {
+    e.append("g")
+    .append("path")
+    .attr("d", path)
+    .attr("class", "area")
+    .attr("fill","steelblue")
+  }
+  
   const 
     width = 960, 
     height = 600,
     geoScale = 1000,
-    transArr = [-1000, 800];
+    transArr = [-1000, 800],
+    radius = 5;
   
 
   const projection = d3
@@ -21,36 +39,23 @@ const buildChart = async () => {
     .attr("viewBox", "0 0 900 800")
     .attr("preserveAspectRatio", "xMidYMid meet");
   
+  //fetch data
   const swiss = await d3.json("./geodata.json")
   const places = await d3.json ("./places.json")
 
-  const cantons = topojson.feature(swiss, swiss.objects.india);
+  //destructure vals
+  const {objects : { india } } = swiss
+  const cantons = topojson.feature(swiss, india);
+  const {features: cantosFeats } = cantos
 
+  //canto datajoin 
   const group =svg.selectAll("g")
-    .data(cantons.features)
-    .enter()
-    .append("g");
-    //.on('mouseover', tip.show)
-        //.on('mouseout', tip.hide)
-
+    .data(cantosFeats)
+    .join(enterGroups)
 
   svg.selectAll(".pin")
     .data(places)
-    .enter().append("circle", ".pin")
-    .attr("r", 5)
-    .attr("transform", function(d) {
-    return "translate(" + projection([
-      d.location.longitude,
-      d.location.latitude
-    ]) + ")";
-    })
-    // .on('mouseover', tip.show)
-    // .on('click', tip.hide);   
-
-  const areas= group.append("path")
-    .attr("d", path)
-    .attr("class", "area")
-   .attr("fill","steelblue");
+    .join(enterPlaces)
                     
 }
 
